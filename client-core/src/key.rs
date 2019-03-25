6480c0bab527b8c866718c821c95be78fc6affd4
Devashish Dixit <47516145+devashishdxt@users.noreply.github.com>
2019-03-25 17:08:54 +0800

    Problem: missing wallet "concept" in client code (CRO-5 CRO-68 #23)
    Solution: initial draft skeleton of client wallet

---
+++ client-core/src/key.rs
@@ -0,0 +1,79 @@
+//! Key management
+use std::fmt;
+
+use bincode::{deserialize, serialize};
+use failure::ResultExt;
+use rand::rngs::OsRng;
+use secp256k1::{PublicKey as SecpPublicKey, SecretKey};
+use zeroize::{Zeroize, ZeroizeOnDrop};
+
+use chain_core::init::address::RedeemAddress;
+
+use crate::SECP;
+use crate::{ErrorKind, Result};
+
+/// Private key used in Crypto.com Chain
+#[derive(Zeroize, ZeroizeOnDrop)]
+pub struct PrivateKey(SecretKey);
+
+impl PrivateKey {
+    /// Generates a new private key
+    pub fn new() -> Result<PrivateKey> {
+        let mut rng = OsRng::new().context(ErrorKind::KeyGenerationError)?;
+        let secret_key = SecretKey::new(&mut rng);
+
+        Ok(PrivateKey(secret_key))
+    }
+
+    /// Serializes current private key
+    pub fn serialize(&self) -> Result<Vec<u8>> {
+        Ok(serialize(&self.0).context(ErrorKind::SerializationError)?)
+    }
+
+    /// Deserializes private key from bytes
+    pub fn deserialize_from(bytes: &[u8]) -> Result<PrivateKey> {
+        let secret_key: SecretKey = deserialize(bytes).context(ErrorKind::DeserializationError)?;
+
+        Ok(PrivateKey(secret_key))
+    }
+}
+
+/// Public key used in Crypto.com Chain
+pub struct PublicKey(SecpPublicKey);
+
+impl PublicKey {
+    /// Serializes current public key
+    pub fn serialize(&self) -> Result<Vec<u8>> {
+        Ok(serialize(&self.0).context(ErrorKind::SerializationError)?)
+    }
+
+    /// Deserializes public key from bytes
+    pub fn deserialize_from(bytes: &[u8]) -> Result<PublicKey> {
+        let public_key: SecpPublicKey =
+            deserialize(bytes).context(ErrorKind::DeserializationError)?;
+
+        Ok(PublicKey(public_key))
+    }
+}
+
+impl fmt::Display for PublicKey {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl From<&PrivateKey> for PublicKey {
+    fn from(private_key: &PrivateKey) -> Self {
+        let secret_key = &private_key.0;
+
+        let public_key = SECP.with(|secp| SecpPublicKey::from_secret_key(secp, secret_key));
+
+        PublicKey(public_key)
+    }
+}
+
+impl From<&PublicKey> for RedeemAddress {
+    fn from(public_key: &PublicKey) -> Self {
+        Self::from(&public_key.0)
+    }
+}
---
+++ client-core/src/service/wallet_service.rs
@@ -0,0 +1,57 @@
+use failure::ResultExt;
+use hex::encode;
+use zeroize::Zeroize;
+
+use chain_core::init::address::RedeemAddress;
+
+use crate::{ErrorKind, PrivateKey, PublicKey, Result, SecureStorage, Storage};
+
+/// Exposes functionality for managing wallets
+pub struct WalletService<T> {
+    storage: T,
+}
+
+impl<T> WalletService<T>
+where
+    T: Storage,
+{
+    /// Creates a new instance of wallet service
+    pub fn new(storage: T) -> Self {
+        WalletService { storage }
+    }
+
+    /// Creates a new wallet and returns wallet ID
+    pub fn create(&self, name: &str, passphrase: &str) -> Result<String> {
+        if self.storage.contains_key(name.as_bytes())? {
+            Err(ErrorKind::AlreadyExists.into())
+        } else {
+            let mut private_key = PrivateKey::new()?;
+            let public_key = PublicKey::from(&private_key);
+
+            private_key.zeroize();
+
+            let address = RedeemAddress::from(&public_key);
+
+            self.storage
+                .set_secure(name.as_bytes(), address.0.to_vec(), passphrase.as_bytes())?;
+
+            Ok(encode(address.0))
+        }
+    }
+
+    /// Retrieves a wallet ID from storage
+    pub fn get(&self, name: &str, passphrase: &str) -> Result<Option<String>> {
+        let address = self
+            .storage
+            .get_secure(name.as_bytes(), passphrase.as_bytes())?;
+
+        match address {
+            None => Ok(None),
+            Some(inner) => {
+                let address =
+                    RedeemAddress::try_from(&inner).context(ErrorKind::DeserializationError)?;
+                Ok(Some(encode(address.0)))
+            }
+        }
+    }
+}
