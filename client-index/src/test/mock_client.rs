18c4625d9507619a4f1d7e095a1368c6be2fbc75
Devashish Dixit <devashish@crypto.com>
2019-04-12 12:52:26 +0800

    Problem: (CRO-54) Client does not store block height and time
    Solution: Added block height and time in transaction storage

--- client-index/src/test/mock_client.rs
+++ client-index/src/test/mock_client.rs
@@ -2,6 +2,8 @@
 
 use std::str::FromStr;
 
+use chrono::DateTime;
+
 use chain_core::init::address::RedeemAddress;
 use chain_core::init::coin::Coin;
 use chain_core::init::config::{ERC20Owner, InitConfig};
@@ -23,6 +25,7 @@ impl Client for MockClient {
     fn genesis(&self) -> Result<Genesis> {
         Ok(Genesis {
             genesis: GenesisInner {
+                genesis_time: DateTime::from_str("2019-04-09T09:33:10.592188Z").unwrap(),
                 chain_id: "test-chain-4UIy1Wab".to_owned(),
                 app_state: InitConfig::new(vec![ERC20Owner::new(
                     RedeemAddress::from_str("0x1fdf22497167a793ca794963ad6c95e6ffa0b971").unwrap(),
@@ -43,6 +46,10 @@ impl Client for MockClient {
     fn block(&self, _: u64) -> Result<Block> {
         Ok(Block {
             block: BlockInner {
+                header: Header {
+                    height: "1".to_owned(),
+                    time: DateTime::from_str("2019-04-09T09:38:41.735577Z").unwrap(),
+                },
                 data: Data {
                     txs: vec!["+JWA+Erj4qBySKi4J+krjuZi++QuAnQITDv9YzjXV0RcDuk+S7pMeIDh4NaAlHkGYaL9naP+5TyquAhZ7K4SWiCliAAA6IkEI8eKw4GrwPhG+ESAAbhASZdu2rJI4Et7q93KedoEsTVFUOCPt8nyY0pGOqixhI4TvORYPVFmJiG+Lsr6L1wmwBLIwxJenWTyKZ8rKrwfkg==".to_owned()]
                 }
