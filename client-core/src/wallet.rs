6963ceac70306b0690454efbf0e4766935fa7725
Devashish Dixit <devashish@crypto.com>
2019-05-03 15:19:53 +0800

    Problem: (CRO-104) Plain insecure strings for passphrases (#70)
    Solution: Started using secstr which provides secure strings.

--- client-core/src/wallet.rs
+++ client-core/src/wallet.rs
@@ -3,6 +3,8 @@ mod default_wallet_client;
 
 pub use default_wallet_client::DefaultWalletClient;
 
+use secstr::SecStr;
+
 use chain_core::init::coin::Coin;
 use chain_core::tx::data::address::ExtendedAddr;
 use chain_core::tx::data::attribute::TxAttributes;
@@ -19,48 +21,49 @@ pub trait WalletClient: Send + Sync {
     fn wallets(&self) -> Result<Vec<String>>;
 
     /// Creates a new wallet with given name and returns wallet_id
-    fn new_wallet(&self, name: &str, passphrase: &str) -> Result<String>;
+    fn new_wallet(&self, name: &str, passphrase: &SecStr) -> Result<String>;
 
     /// Retrieves all public keys corresponding to given wallet
-    fn private_keys(&self, name: &str, passphrase: &str) -> Result<Vec<PrivateKey>>;
+    fn private_keys(&self, name: &str, passphrase: &SecStr) -> Result<Vec<PrivateKey>>;
 
     /// Retrieves all public keys corresponding to given wallet
-    fn public_keys(&self, name: &str, passphrase: &str) -> Result<Vec<PublicKey>>;
+    fn public_keys(&self, name: &str, passphrase: &SecStr) -> Result<Vec<PublicKey>>;
 
     /// Retrieves all addresses corresponding to given wallet
-    fn addresses(&self, name: &str, passphrase: &str) -> Result<Vec<ExtendedAddr>>;
+    fn addresses(&self, name: &str, passphrase: &SecStr) -> Result<Vec<ExtendedAddr>>;
 
     /// Retrieves private key corresponding to given address
     fn private_key(
         &self,
         name: &str,
-        passphrase: &str,
+        passphrase: &SecStr,
         address: &ExtendedAddr,
     ) -> Result<Option<PrivateKey>>;
 
     /// Generates a new public key for given wallet
-    fn new_public_key(&self, name: &str, passphrase: &str) -> Result<PublicKey>;
+    fn new_public_key(&self, name: &str, passphrase: &SecStr) -> Result<PublicKey>;
 
     /// Generates a new address for given wallet
-    fn new_address(&self, name: &str, passphrase: &str) -> Result<ExtendedAddr>;
+    fn new_address(&self, name: &str, passphrase: &SecStr) -> Result<ExtendedAddr>;
 
     /// Retrieves current balance of wallet
-    fn balance(&self, name: &str, passphrase: &str) -> Result<Coin>;
+    fn balance(&self, name: &str, passphrase: &SecStr) -> Result<Coin>;
 
     /// Retrieves transaction history of wallet
-    fn history(&self, name: &str, passphrase: &str) -> Result<Vec<TransactionChange>>;
+    fn history(&self, name: &str, passphrase: &SecStr) -> Result<Vec<TransactionChange>>;
 
     /// Creates and broadcasts a transaction to Crypto.com Chain
     fn create_and_broadcast_transaction(
         &self,
         name: &str,
-        passphrase: &str,
+        passphrase: &SecStr,
         outputs: Vec<TxOut>,
         attributes: TxAttributes,
     ) -> Result<()>;
 
     /// Broadcasts a transaction to Crypto.com Chain
-    fn broadcast_transaction(&self, name: &str, passphrase: &str, transaction: Tx) -> Result<()>;
+    fn broadcast_transaction(&self, name: &str, passphrase: &SecStr, transaction: Tx)
+        -> Result<()>;
 
     /// Synchronizes index with Crypto.com Chain (from last known height)
     fn sync(&self) -> Result<()>;
