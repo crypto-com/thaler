4aa21c6156b75f9478a3e4ab9a3b9f8f9382c79c
Jongwhan Lee <51560997+leejw51crypto@users.noreply.github.com>
2019-06-21 10:17:20 +0800

    Problem: client can't create unbond stake and withdraw unbonded stake transactions (CRO-174) (#154)
    Solution: added two function implementation to DefaultNetworkOpsClient

--- client-network/src/network_ops.rs
+++ client-network/src/network_ops.rs
@@ -3,16 +3,36 @@ mod default_network_ops_client;
 
 pub use self::default_network_ops_client::DefaultNetworkOpsClient;
 
-use secstr::SecUtf8;
-
+use chain_core::init::coin::Coin;
+use chain_core::state::account::{StakedStateAddress, StakedStateOpAttributes};
 use chain_core::tx::data::address::ExtendedAddr;
 use chain_core::tx::data::attribute::TxAttributes;
+use chain_core::tx::data::input::TxoPointer;
 use chain_core::tx::data::output::TxOut;
 use chain_core::tx::TxAux;
 use client_common::Result;
+use secstr::SecUtf8;
 
 /// Interface for performing network operations on Crypto.com Chain
 pub trait NetworkOpsClient {
+    /// creates a new transaction for bonding stake transaction with utxos
+    fn create_deposit_bonded_stake_transaction(
+        &self,
+        name: &str,
+        passphrase: &SecUtf8,
+        inputs: Vec<TxoPointer>,
+        to_staked_account: StakedStateAddress,
+        attributes: StakedStateOpAttributes,
+    ) -> Result<TxAux>;
+    /// creates a new transaction for unbonding stake transaction
+    fn create_unbond_stake_transaction(
+        &self,
+        name: &str,
+        passphrase: &SecUtf8,
+        from_address: &ExtendedAddr,
+        value: Coin,
+        attributes: StakedStateOpAttributes,
+    ) -> Result<TxAux>;
     /// Creates a new transaction for withdrawing unbonded stake from an account
     fn create_withdraw_unbonded_stake_transaction(
         &self,
@@ -23,5 +43,5 @@ pub trait NetworkOpsClient {
         attributes: TxAttributes,
     ) -> Result<TxAux>;
 
-    // TODO: Add `create_deposit_bonded_stake_transaction()`, `create_unbond_stake_transaction()` and `get_account_details()` functions
+    // TODO: Add `get_account_details()` functions
 }
--- client-network/src/network_ops/default_network_ops_client.rs
+++ client-network/src/network_ops/default_network_ops_client.rs
@@ -1,37 +1,105 @@
-use secstr::SecUtf8;
-
+use crate::NetworkOpsClient;
+use chain_core::init::coin::Coin;
+use chain_core::state::account::{DepositBondTx, UnbondTx};
+use chain_core::state::account::{StakedStateAddress, StakedStateOpAttributes};
 use chain_core::state::account::{StakedStateOpWitness, WithdrawUnbondedTx};
 use chain_core::tx::data::address::ExtendedAddr;
 use chain_core::tx::data::attribute::TxAttributes;
+use chain_core::tx::data::input::TxoPointer;
 use chain_core::tx::data::output::TxOut;
 use chain_core::tx::{TransactionId, TxAux};
 use client_common::{Error, ErrorKind, Result};
+use client_core::signer::Signer;
+use client_core::UnspentTransactions;
 use client_core::WalletClient;
-
-use crate::NetworkOpsClient;
+use secstr::SecUtf8;
 
 /// Default implementation of `NetworkOpsClient`
-pub struct DefaultNetworkOpsClient<'a, W>
+pub struct DefaultNetworkOpsClient<'a, W, S>
 where
     W: WalletClient,
+    S: Signer,
 {
     wallet_client: &'a W,
+    signer: &'a S,
 }
 
-impl<'a, W> DefaultNetworkOpsClient<'a, W>
+impl<'a, W, S> DefaultNetworkOpsClient<'a, W, S>
 where
     W: WalletClient,
+    S: Signer,
 {
     /// Creates a new instance of `DefaultNetworkOpsClient`
-    pub fn new(wallet_client: &'a W) -> Self {
-        Self { wallet_client }
+    pub fn new(wallet_client: &'a W, signer: &'a S) -> Self {
+        Self {
+            wallet_client,
+            signer,
+        }
     }
 }
 
-impl<'a, W> NetworkOpsClient for DefaultNetworkOpsClient<'a, W>
+impl<'a, W, S> NetworkOpsClient for DefaultNetworkOpsClient<'a, W, S>
 where
     W: WalletClient,
+    S: Signer,
 {
+    fn create_deposit_bonded_stake_transaction(
+        &self,
+        name: &str,
+        passphrase: &SecUtf8,
+        inputs: Vec<TxoPointer>,
+        to_staked_account: StakedStateAddress,
+        attributes: StakedStateOpAttributes,
+    ) -> Result<TxAux> {
+        let transaction: DepositBondTx =
+            DepositBondTx::new(inputs.clone(), to_staked_account, attributes);
+
+        let transactions = inputs
+            .into_iter()
+            .map(|txo_pointer: TxoPointer| {
+                let id = txo_pointer.id;
+                let index = txo_pointer.index;
+                Ok((txo_pointer, self.wallet_client.output(&id, index as usize)?))
+            })
+            .collect::<Result<Vec<(TxoPointer, TxOut)>>>()?;
+        let unspent_transactions = UnspentTransactions::new(transactions);
+        let witness = self.signer.sign(
+            name,
+            passphrase,
+            transaction.id(),
+            unspent_transactions.select_all(),
+        )?;
+        Ok(TxAux::DepositStakeTx(transaction, witness))
+    }
+    fn create_unbond_stake_transaction(
+        &self,
+        name: &str,
+        passphrase: &SecUtf8,
+        from_address: &ExtendedAddr,
+        value: Coin,
+        attributes: StakedStateOpAttributes,
+    ) -> Result<TxAux> {
+        match from_address {
+            ExtendedAddr::BasicRedeem(ref redeem_address) => {
+                let transaction = UnbondTx::new(value, 0, attributes);
+                let public_key = self
+                    .wallet_client
+                    .find_public_key(name, passphrase, redeem_address)?
+                    .ok_or_else(|| Error::from(ErrorKind::AddressNotFound))?;
+
+                let private_key = self
+                    .wallet_client
+                    .private_key(passphrase, &public_key)?
+                    .ok_or_else(|| Error::from(ErrorKind::PrivateKeyNotFound))?;
+                let signature = private_key
+                    .sign(transaction.id())
+                    .map(StakedStateOpWitness::new)?;
+                Ok(TxAux::UnbondStakeTx(transaction, signature))
+            }
+            ExtendedAddr::OrTree(_) => Err(ErrorKind::InvalidInput.into()),
+        }
+    }
+
     fn create_withdraw_unbonded_stake_transaction(
         &self,
         name: &str,
@@ -42,26 +110,18 @@ where
     ) -> Result<TxAux> {
         match from_address {
             ExtendedAddr::BasicRedeem(ref redeem_address) => {
-                let transaction = WithdrawUnbondedTx {
-                    nonce: 0,
-                    outputs,
-                    attributes,
-                };
-
+                let transaction = WithdrawUnbondedTx::new(0, outputs, attributes);
                 let public_key = self
                     .wallet_client
                     .find_public_key(name, passphrase, redeem_address)?
                     .ok_or_else(|| Error::from(ErrorKind::AddressNotFound))?;
-
                 let private_key = self
                     .wallet_client
                     .private_key(passphrase, &public_key)?
                     .ok_or_else(|| Error::from(ErrorKind::PrivateKeyNotFound))?;
-
                 let signature = private_key
                     .sign(transaction.id())
                     .map(StakedStateOpWitness::new)?;
-
                 Ok(TxAux::WithdrawUnbondedStakeTx(transaction, signature))
             }
             ExtendedAddr::OrTree(_) => Err(ErrorKind::InvalidInput.into()),
@@ -78,8 +138,73 @@ mod tests {
 
     use chain_tx_validation::witness::verify_tx_recover_address;
     use client_common::storage::MemoryStorage;
+    use client_core::signer::DefaultSigner;
     use client_core::wallet::DefaultWalletClient;
     use client_core::{PrivateKey, PublicKey};
+    use std::str::FromStr;
+
+    #[test]
+    fn check_create_deposit_bonded_stake_transaction() {
+        let name = "name";
+        let passphrase = &SecUtf8::from("passphrase");
+
+        let storage = MemoryStorage::default();
+        let signer = DefaultSigner::new(storage.clone());
+
+        let wallet_client = DefaultWalletClient::builder()
+            .with_wallet(storage)
+            .build()
+            .unwrap();
+
+        let network_ops_client = DefaultNetworkOpsClient::new(&wallet_client, &signer);
+
+        let inputs: Vec<TxoPointer> = vec![];
+        let to_staked_account =
+            RedeemAddress::from_str("1fdf22497167a793ca794963ad6c95e6ffa0b971").unwrap();
+
+        let attributes = StakedStateOpAttributes::new(0);
+        assert!(network_ops_client
+            .create_deposit_bonded_stake_transaction(
+                name,
+                passphrase,
+                inputs,
+                to_staked_account.into(),
+                attributes,
+            )
+            .is_ok());
+    }
+
+    #[test]
+    fn check_create_unbond_stake_transaction() {
+        let name = "name";
+        let passphrase = &SecUtf8::from("passphrase");
+
+        let storage = MemoryStorage::default();
+        let signer = DefaultSigner::new(storage.clone());
+
+        let wallet_client = DefaultWalletClient::builder()
+            .with_wallet(storage)
+            .build()
+            .unwrap();
+
+        let network_ops_client = DefaultNetworkOpsClient::new(&wallet_client, &signer);
+
+        let value = Coin::new(0).unwrap();
+        let attributes = StakedStateOpAttributes::new(0);
+        assert_eq!(
+            ErrorKind::InvalidInput,
+            network_ops_client
+                .create_unbond_stake_transaction(
+                    name,
+                    passphrase,
+                    &ExtendedAddr::OrTree([0; 32]),
+                    value,
+                    attributes,
+                )
+                .unwrap_err()
+                .kind()
+        );
+    }
 
     #[test]
     fn check_withdraw_unbonded_stake_transaction() {
@@ -87,11 +212,14 @@ mod tests {
         let passphrase = &SecUtf8::from("passphrase");
 
         let storage = MemoryStorage::default();
+        let signer = DefaultSigner::new(storage.clone());
+
         let wallet_client = DefaultWalletClient::builder()
             .with_wallet(storage)
             .build()
             .unwrap();
-        let network_ops_client = DefaultNetworkOpsClient::new(&wallet_client);
+
+        let network_ops_client = DefaultNetworkOpsClient::new(&wallet_client, &signer);
 
         wallet_client.new_wallet(name, passphrase).unwrap();
 
@@ -127,11 +255,14 @@ mod tests {
         let passphrase = &SecUtf8::from("passphrase");
 
         let storage = MemoryStorage::default();
+        let signer = DefaultSigner::new(storage.clone());
+
         let wallet_client = DefaultWalletClient::builder()
             .with_wallet(storage)
             .build()
             .unwrap();
-        let network_ops_client = DefaultNetworkOpsClient::new(&wallet_client);
+
+        let network_ops_client = DefaultNetworkOpsClient::new(&wallet_client, &signer);
 
         wallet_client.new_wallet(name, passphrase).unwrap();
 
@@ -158,11 +289,14 @@ mod tests {
         let passphrase = &SecUtf8::from("passphrase");
 
         let storage = MemoryStorage::default();
+        let signer = DefaultSigner::new(storage.clone());
+
         let wallet_client = DefaultWalletClient::builder()
             .with_wallet(storage)
             .build()
             .unwrap();
-        let network_ops_client = DefaultNetworkOpsClient::new(&wallet_client);
+
+        let network_ops_client = DefaultNetworkOpsClient::new(&wallet_client, &signer);
 
         assert_eq!(
             ErrorKind::WalletNotFound,
@@ -187,11 +321,14 @@ mod tests {
         let passphrase = &SecUtf8::from("passphrase");
 
         let storage = MemoryStorage::default();
+        let signer = DefaultSigner::new(storage.clone());
+
         let wallet_client = DefaultWalletClient::builder()
             .with_wallet(storage)
             .build()
             .unwrap();
-        let network_ops_client = DefaultNetworkOpsClient::new(&wallet_client);
+
+        let network_ops_client = DefaultNetworkOpsClient::new(&wallet_client, &signer);
 
         assert_eq!(
             ErrorKind::InvalidInput,
