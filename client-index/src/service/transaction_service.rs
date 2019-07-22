95937316bbf8daf112fb914dc49e6634b4dc0e18
Devashish Dixit <devashish@crypto.com>
2019-07-22 13:47:27 +0800

    Problem: (CRO-219) Potential inconsistent client state due to concurrent updates
    Solution:
    
    - Combined `balance`, `transaction_history` and `unspent_transactions` in a single keyspace.
    - Use a simplified variant of [memento](https://refactoring.guru/design-patterns/memento) design pattern to buffer all database updates and execute them in the end.

--- client-index/src/service/transaction_service.rs
+++ client-index/src/service/transaction_service.rs
@@ -1,15 +1,23 @@
+use std::convert::TryInto;
+
 use parity_codec::{Decode, Encode};
 
+use chain_core::tx::data::input::TxoPointer;
+use chain_core::tx::data::output::TxOut;
 use chain_core::tx::data::TxId;
-use client_common::{Result, Storage, Transaction};
+use chain_core::tx::TransactionId;
+use client_common::{Error, ErrorKind, Result, Storage, Transaction};
 
 const KEYSPACE: &str = "index_transaction";
 
-/// Exposes functionalities for managing transactions
+/// Exposes functionalities for managing transaction storage
 ///
 /// Stores `transaction_id -> transaction` mapping
 #[derive(Default, Clone)]
-pub struct TransactionService<S: Storage> {
+pub struct TransactionService<S>
+where
+    S: Storage,
+{
     storage: S,
 }
 
@@ -18,6 +26,7 @@ where
     S: Storage,
 {
     /// Creates a new instance of transaction service
+    #[inline]
     pub fn new(storage: S) -> Self {
         Self { storage }
     }
@@ -33,13 +42,38 @@ where
     }
 
     /// Sets transaction with given id and value
-    pub fn set(&self, id: &TxId, transaction: &Transaction) -> Result<()> {
-        self.storage.set(KEYSPACE, id, transaction.encode())?;
+    pub fn set(&self, transaction: &Transaction) -> Result<()> {
+        self.storage
+            .set(KEYSPACE, &transaction.id(), transaction.encode())
+            .map(|_| ())
+    }
 
-        Ok(())
+    /// Retrieves transaction output corresponding to given pointer
+    pub fn get_output(&self, input: &TxoPointer) -> Result<TxOut> {
+        let transaction = self
+            .get(&input.id)?
+            .ok_or_else(|| Error::from(ErrorKind::TransactionNotFound))?;
+
+        let outputs = match transaction {
+            Transaction::TransferTransaction(transfer_transaction) => {
+                Ok(transfer_transaction.outputs)
+            }
+            Transaction::WithdrawUnbondedStakeTransaction(withdraw_transaction) => {
+                Ok(withdraw_transaction.outputs)
+            }
+            _ => Err(Error::from(ErrorKind::InvalidTransaction)),
+        }?;
+
+        let output = outputs
+            .into_iter()
+            .nth(input.index.try_into().unwrap())
+            .ok_or_else(|| Error::from(ErrorKind::TransactionNotFound))?;
+
+        Ok(output)
     }
 
     /// Clears all storage
+    #[inline]
     pub fn clear(&self) -> Result<()> {
         self.storage.clear(KEYSPACE)
     }
@@ -49,19 +83,48 @@ where
 mod tests {
     use super::*;
 
+    use chain_core::init::coin::Coin;
+    use chain_core::tx::data::address::ExtendedAddr;
+    use chain_core::tx::data::attribute::TxAttributes;
     use chain_core::tx::data::Tx;
     use client_common::storage::MemoryStorage;
 
+    fn transfer_transaction() -> Transaction {
+        Transaction::TransferTransaction(Tx::new_with(
+            Vec::new(),
+            vec![TxOut::new(
+                ExtendedAddr::OrTree([0; 32]),
+                Coin::new(100).unwrap(),
+            )],
+            TxAttributes::default(),
+        ))
+    }
+
     #[test]
     fn check_flow() {
-        let transaction_service = TransactionService::new(MemoryStorage::default());
-        let id = [0u8; 32];
-        let transaction = Transaction::TransferTransaction(Tx::default());
+        let storage = MemoryStorage::default();
 
-        assert_eq!(None, transaction_service.get(&id).unwrap());
-        assert!(transaction_service.set(&id, &transaction).is_ok());
-        assert_eq!(transaction, transaction_service.get(&id).unwrap().unwrap());
+        let transaction_service = TransactionService::new(storage);
+        let transaction = transfer_transaction();
+        let transaction_id = transaction.id();
+
+        assert_eq!(None, transaction_service.get(&transaction_id).unwrap());
+        assert!(transaction_service.set(&transaction).is_ok());
+
+        assert_eq!(
+            transaction,
+            transaction_service.get(&transaction_id).unwrap().unwrap()
+        );
+
+        assert_eq!(
+            Coin::new(100).unwrap(),
+            transaction_service
+                .get_output(&TxoPointer::new(transaction_id, 0))
+                .unwrap()
+                .value
+        );
+
         assert!(transaction_service.clear().is_ok());
-        assert_eq!(None, transaction_service.get(&id).unwrap());
+        assert_eq!(None, transaction_service.get(&transaction_id).unwrap());
     }
 }
