1228570086f297e558d1f0af837fd7453245088e
Devashish Dixit <devashish@crypto.com>
2019-07-24 11:25:47 +0800

    Problem: (CRO-215) Client does not handle unencrypted transaction in a block (deposit and unbond) (#227)
    Solution: Modified `BlockHandler` and `ManualSynchronizer` to handle unencrypted transactions
    
    * Fixed handling on invalid transactions and added tests
    
    * Changed implementation of filtering unencrypted transactions

--- client-common/src/block_header.rs
+++ client-common/src/block_header.rs
@@ -3,6 +3,8 @@ use chrono::{DateTime, Utc};
 use chain_core::tx::data::TxId;
 use chain_tx_filter::BlockFilter;
 
+use crate::Transaction;
+
 /// Structure for representing a block header on Crypto.com Chain
 pub struct BlockHeader {
     /// Block height
@@ -11,6 +13,8 @@ pub struct BlockHeader {
     pub block_time: DateTime<Utc>,
     /// List of successfully committed transaction ids in this block
     pub transaction_ids: Vec<TxId>,
-    /// Bloom filter for view keys
-    pub view_key_filter: BlockFilter,
+    /// Bloom filter for view keys and staking addresses
+    pub block_filter: BlockFilter,
+    /// List of un-encrypted transactions (only contains transactions of type `DepositStake` and `UnbondStake`)
+    pub unencrypted_transactions: Vec<Transaction>,
 }
