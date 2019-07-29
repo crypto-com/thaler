6e3fef597ade0e663d2511a9a4d098758aa93b86
Calvin Lau <calvin@mona.co>
2019-07-29 17:08:55 +0800

    🐳 Update Docker Compose file

--- integration-tests/prepare.sh
+++ integration-tests/prepare.sh
@@ -4,15 +4,17 @@ IFS=
 
 TENDERMINT_PATH=${TENDERMINT_PATH:-tendermint}
 WALLET_PASSPHRASE=${WALLET_PASSPHRASE:-123456}
-TENDERMINT_VERSION=${TENDERMINT_VERSION:-latest}
+TENDERMINT_VERSION=${TENDERMINT_VERSION:-0.30.4}
 
-# Do not modify the following constants
-WALLET_TEMP_PATH="./chain/wallet-storage"
+# Constants (No not modify unless you are absolutely sure what you are doing)
+WALLET_STORAGE_DIRECTORY="./docker/chain/wallet-storage"
+ADDRESS_STATE_PATH="./address-state.json"
 DEV_CONF_WITHFEE_PATH="./dev-conf-withfee.json"
 DEV_CONF_ZEROFEE_PATH="./dev-conf-zerofee.json"
 TENDERMINT_TEMP_DIRECTORY="./tendermint"
 TENDERMINT_WITHFEE_DIRECTORY="./docker/tendermint/tendermint-withfee"
 TENDERMINT_ZEROFEE_DIRECTORY="./docker/tendermint/tendermint-zerofee"
+CHAIN_ID="test-chain-y3m1e6-AB"
 
 # Global function return value
 RET_VALUE=0
@@ -41,7 +43,7 @@ function print_error() {
 # @argument Command to test
 function check_command_exist() {
     set +e
-    command -v ${1}
+    command -v ${1} > /dev/null
     if [ x"$?" = "x1" ]; then
         print_error "command not found: ${1}"
         exit 1
@@ -53,22 +55,58 @@ function check_command_exist() {
 # @argument Wallet Name
 # @argument Wallet Passphrase
 function create_wallet() {
+    rm -rf "${WALLET_STORAGE_DIRECTORY}"
     print_step "Creating wallet \"${1}\""
-    RET_VALUE=$(printf "${2}\n" | ../target/debug/client-cli wallet new --name ${1})
+    RET_VALUE=$(printf "${2}\n" | CRYPTO_CLIENT_STORAGE=${WALLET_STORAGE_DIRECTORY} ../target/debug/client-cli wallet new --name ${1})
 }
 
 # Create wallet staking address
 # @argument Wallet Name
 # @argument Wallet Passphrase
 function create_wallet_staking_address() {
-    print_step "Creating address for wallet \"${1}\""
-    printf "${2}\n" | ../target/debug/client-cli address new --name ${1} --type Staking
+    print_step "Creating staking address for wallet \"${1}\""
+    printf "${2}\n" | CRYPTO_CLIENT_STORAGE=${WALLET_STORAGE_DIRECTORY} ../target/debug/client-cli address new --name ${1} --type Staking
 
-    print_step "Retrieving last address for wallet \"${1}\""
-    ADDRESS_LIST=$(printf "${2}\n" | ../target/debug/client-cli address list --name ${1} --type Staking)
+    print_step "Retrieving last staking address for wallet \"${1}\""
+    ADDRESS_LIST=$(printf "${2}\n" | CRYPTO_CLIENT_STORAGE=${WALLET_STORAGE_DIRECTORY} ../target/debug/client-cli address list --name ${1} --type Staking)
     echo "${ADDRESS_LIST}"
     RET_VALUE=$(echo $ADDRESS_LIST | tail -n2 | sed -En "s/^.*(0x[0-9a-zA-Z]+)/\1/p")
 }
+
+# Create wallet staking address
+# @argument Wallet Name
+# @argument Wallet Passphrase
+function create_wallet_transfer_address() {
+    print_step "Creating transfer address for wallet \"${1}\""
+    printf "${2}\n" | CRYPTO_CLIENT_STORAGE=${WALLET_STORAGE_DIRECTORY} ../target/debug/client-cli address new --name ${1} --type Transfer
+
+    print_step "Retrieving last transfer address for wallet \"${1}\""
+    ADDRESS_LIST=$(printf "${2}\n" | CRYPTO_CLIENT_STORAGE=${WALLET_STORAGE_DIRECTORY} ../target/debug/client-cli address list --name ${1} --type Transfer)
+    echo "${ADDRESS_LIST}"
+    RET_VALUE=$(echo $ADDRESS_LIST | tail -n1 | sed -En "s/^.*(0x[0-9a-zA-Z]+)/\1/p")
+}
+
+# Save wallet addresses into JSON file
+# @argument Staking address
+# @argument Transfer address 1
+# @argument Transfer address 1
+function save_wallet_addresses() {
+    echo "${ADDRESS_STATE_TEMPLATE}" | \
+        jq --arg ADDRESS "${1}" '.staking=($ADDRESS)' | \
+        jq --arg ADDRESS "${2}" '.transfer[0]=($ADDRESS)' | \
+        jq --arg ADDRESS "${3}" '.transfer[1]=($ADDRESS)' > ${ADDRESS_STATE_PATH}
+}
+
+ADDRESS_STATE_TEMPLATE=$(cat << EOF
+{
+	"staking": "{STAKING_ADDRESS}",
+    "transfer": [
+        "{TRANSFER_ADDRESS_1}",
+        "{TRANSFER_ADDRESS_2}"
+    ]
+}
+EOF
+)
 
 DEV_CONF=$(cat << EOF
 {
@@ -102,10 +140,16 @@ EOF
 
 # @argument Dev uilts config path
 # @argument Tendermint directory
+# @argument Chain ID
 function generate_tendermint_genesis() {
     print_step "Generating Tendermint genesis ${1} -> ${2}"
     _generate_genesis "${1}"; GENESIS_JSON="${RET_VALUE}"
-    _append_genesis_to_tendermint_genesis "${2}" "${GENESIS_JSON}"
+
+    TENDERMINT_GENESIS_JSON=$(cat "${2}/config/genesis.json")
+    _append_genesis_to_tendermint_genesis "${TENDERMINT_GENESIS_JSON}" "${GENESIS_JSON}"
+    _change_tenermint_chain_id "${RET_VALUE}" "${3}"
+
+    echo "${RET_VALUE}" > "${2}/config/genesis.json"
 }
 
 # @argument Dev uilts config path
@@ -113,17 +157,23 @@ function _generate_genesis() {
     RET_VALUE=$(../target/debug/dev-utils genesis generate -g "${1}")
 }
 
-# @argument Tendermint directory
+# @argument Tendermint genesis JSON
 # @argument Genesis JSON generated
 function _append_genesis_to_tendermint_genesis() {
     # Find the line of occurence of last }
-    APP_HASH_LINE_NUMBER=$(cat "${1}/config/genesis.json" | awk '/app_hash/{print NR}' | tail -n1)
+    APP_HASH_LINE_NUMBER=$(echo "${1}" | awk '/app_hash/{print NR}' | tail -n1)
     # Append generated JSON after the line
-    GENESIS_PREFIX=$(cat "${1}/config/genesis.json" | head -n$(($APP_HASH_LINE_NUMBER - 1)))
-    echo "${GENESIS_PREFIX}${2}}" > "${1}/config/genesis.json"
+    GENESIS_PREFIX=$(echo "${1}" | head -n$(($APP_HASH_LINE_NUMBER - 1)))
+    RET_VALUE="${GENESIS_PREFIX}${2}}"
 }
 
-# Change working directory
+# @argument Tendermint genesis JSON
+# @argument Chain ID
+function _change_tenermint_chain_id() {
+    RET_VALUE=$(echo "${1}" | jq --arg CHAIN_ID "${CHAIN_ID}" '.chain_id=($CHAIN_ID)')
+}
+
+# Always execute at script located directory
 cd "$(dirname "${0}")"
 
 check_command_exist "jq"
@@ -138,12 +188,19 @@ print_step "Clone Tendermint configuration"
 mkdir -p "${TENDERMINT_WITHFEE_DIRECTORY}"; cp -r ./tendermint/. "${TENDERMINT_WITHFEE_DIRECTORY}"
 mkdir -p "${TENDERMINT_ZEROFEE_DIRECTORY}"; cp -r ./tendermint/. "${TENDERMINT_ZEROFEE_DIRECTORY}"
 
+print_step "Generate wallet and addresses"
 create_wallet "Default" "${WALLET_PASSPHRASE}"
 
 create_wallet_staking_address "Default" "${WALLET_PASSPHRASE}"; STAKING_ADDRESS="${RET_VALUE}"
+create_wallet_transfer_address "Default" "${WALLET_PASSPHRASE}"; TRANSFER_ADDRESS_1="${RET_VALUE}"
+create_wallet_transfer_address "Default" "${WALLET_PASSPHRASE}"; TRANSFER_ADDRESS_2="${RET_VALUE}"
+print_config "STAKING_ADDRESS" "${STAKING_ADDRESS}"
+print_config "TRANSFER_ADDRESS_1" "${TRANSFER_ADDRESS_1}"
+print_config "TRANSFER_ADDRESS_2" "${TRANSFER_ADDRESS_2}"
+save_wallet_addresses "${STAKING_ADDRESS}" "${TRANSFER_ADDRESS_1}" "${TRANSFER_ADDRESS_2}"
+
 VALIDATOR_PUB_KEY=$(cat ~/.tendermint/config/genesis.json | jq -r .validators[0].pub_key.value)
 GENESIS_TIME=$(cat ~/.tendermint/config/genesis.json | jq -r .genesis_time)
-print_config "STAKING_ADDRESS" "${STAKING_ADDRESS}"
 print_config "VALIDATOR_PUR_KEY" "${VALIDATOR_PUB_KEY}"
 print_config "GENESIS_TIME" "${GENESIS_TIME}"
 
@@ -160,9 +217,10 @@ function generate_dev_conf() {
 		sed "s/{GENESIS_TIME}/${GENESIS_TIME}/g" > ${3}
 }
 
+print_step "Generate Tendermint genesis"
 generate_dev_conf "1.1" "1.25" "${DEV_CONF_WITHFEE_PATH}"
 generate_dev_conf "0.0" "0.0" "${DEV_CONF_ZEROFEE_PATH}"
 
-generate_tendermint_genesis "${DEV_CONF_WITHFEE_PATH}" "${TENDERMINT_WITHFEE_DIRECTORY}"
-generate_tendermint_genesis "${DEV_CONF_ZEROFEE_PATH}" "${TENDERMINT_ZEROFEE_DIRECTORY}"
+generate_tendermint_genesis "${DEV_CONF_WITHFEE_PATH}" "${TENDERMINT_WITHFEE_DIRECTORY}" "${CHAIN_ID}"
+generate_tendermint_genesis "${DEV_CONF_ZEROFEE_PATH}" "${TENDERMINT_ZEROFEE_DIRECTORY}" "${CHAIN_ID}"
 
