771399bb3ee3108ca5276f2e63bcdfa5ff3bcb05
jongwhan lee <leejw51@gmail.com>
2019-07-31 09:34:35 +0000

    now working

--- dev-utils/src/commands/init_command.rs
+++ dev-utils/src/commands/init_command.rs
@@ -3,20 +3,20 @@ use super::genesis_dev_config::GenesisDevConfig;
 use chain_core::init::config::{InitialValidator, ValidatorKeyType};
 use chain_core::init::{address::RedeemAddress, coin::Coin};
 use chrono::DateTime;
+use client_common::storage::SledStorage;
+use client_core::wallet::{DefaultWalletClient, WalletClient};
 use failure::ResultExt;
 use failure::{format_err, Error};
-use quest::{ask, error, password};
+use quest::{ask, error, password, success};
 use read_input::prelude::*;
 use secstr::SecUtf8;
 use serde_json::json;
 use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
-use client_common::storage::SledStorage;
 use std::process::Command;
 use std::str::FromStr;
 use std::{env, io};
-use client_core::wallet::{DefaultWalletClient, WalletClient};
 
 use std::io::{Read, Write};
 
@@ -47,7 +47,6 @@ impl InitCommand {
     }
 
     pub fn read_wallet(&mut self, id: &str, default1: &str, default2: &str) {
-        let distribution = &mut self.genesis_dev.distribution;
         let a = input()
             .msg(format!("wallet {}({}) address=", id, default1))
             .default(default1.to_string())
@@ -56,30 +55,38 @@ impl InitCommand {
             .msg(format!("wallet {}({}) amount=", id, default2))
             .default(default2.to_string())
             .get();
+
+        self.do_read_wallet(a, b);
+    }
+    pub fn do_read_wallet(&mut self, a: String, b: String) {
+        let distribution = &mut self.genesis_dev.distribution;
         let b2 = Coin::from_str(&b).unwrap();
+        println!(
+            "do_read_wallet={}",
+            RedeemAddress::from_str(&a).unwrap().to_string()
+        );
         distribution.insert(RedeemAddress::from_str(&a).unwrap(), b2);
         self.remain_coin = (self.remain_coin - b2).unwrap();
         self.distribution_addresses.push(a.to_string());
     }
     pub fn read_information(&mut self) -> Result<(), Error> {
         let default_address = RedeemAddress::default().to_string();
-        self.staking_account_address = input()
-            .msg(format!(
-                "Please staking_account_address({})=",
-                default_address
-            ))
-            .default(default_address.clone())
-            .get();
         println!(
             "maximum coin to distribute={}",
             self.remain_coin.get_string()
+        );
+
+        assert!(42 == self.staking_account_address.len());
+        self.do_read_wallet(
+            self.staking_account_address.clone(),
+            "1250000000000000000".to_string(),
         );
 
         loop {
             if self.remain_coin == Coin::zero() {
                 break;
             }
-            let i = self.distribution_addresses.len() + 1;
+            let i = self.distribution_addresses.len();
             self.read_wallet(
                 format!("{}", i).as_str(),
                 default_address.as_str(),
@@ -99,6 +106,10 @@ impl InitCommand {
 
             // save
             let councils = &mut self.genesis_dev.council_nodes;
+            println!(
+                "{} {}",
+                self.staking_account_address, self.tendermint_pubkey
+            );
             let staking_validator = InitialValidator {
                 staking_account_address: self
                     .staking_account_address
@@ -109,14 +120,15 @@ impl InitCommand {
             };
 
             councils.push(staking_validator);
+            assert!(self.distribution_addresses.len() >= 4);
 
             self.genesis_dev.launch_incentive_from = RedeemAddress::from_str(
                 &input()
                     .msg(format!(
                         "launch_incentive_from({})=",
-                        self.distribution_addresses[0]
+                        self.distribution_addresses[1]
                     ))
-                    .default(self.distribution_addresses[0].clone())
+                    .default(self.distribution_addresses[1].clone())
                     .get(),
             )
             .unwrap();
@@ -124,9 +136,9 @@ impl InitCommand {
                 &input()
                     .msg(format!(
                         "launch_incentive_to({})=",
-                        self.distribution_addresses[1]
+                        self.distribution_addresses[2]
                     ))
-                    .default(self.distribution_addresses[1].clone())
+                    .default(self.distribution_addresses[2].clone())
                     .get(),
             )
             .unwrap();
@@ -134,9 +146,9 @@ impl InitCommand {
                 &input()
                     .msg(format!(
                         "long_term_incentive({})=",
-                        self.distribution_addresses[2]
+                        self.distribution_addresses[3]
                     ))
-                    .default(self.distribution_addresses[2].clone())
+                    .default(self.distribution_addresses[3].clone())
                     .get(),
             )
             .unwrap();
@@ -216,21 +228,41 @@ impl InitCommand {
             })
             .map(|_e| ())
     }
-    pub fn execute(&mut self) -> Result<(), Error> {
-        println!("initialize");
-
-      /*  let storage = SledStorage::new(InitCommand::storage_path())?;
+    pub fn read_staking_address(&mut self) -> Result<(), Error> {
+        let storage = SledStorage::new(InitCommand::storage_path())?;
         let wallet_client = DefaultWalletClient::builder()
             .with_wallet(storage)
-            .build()?;*/
+            .build()?;
 
-        //self.prepare_tendermint()
-          self.prepare_tendermint()
-        .and_then(|_| self.read_tendermint_genesis())
-        .and_then(|_| self.read_information())
-        .and_then(|_| self.generate_app_info())
-        .and_then(|_| self.write_tendermint_genesis())
-        .map_err(|e| format_err!("init error={}", e))
+        let name = input()
+            .msg("please enter wallet name=")
+            .default("my".to_string())
+            .get();
+        let passphrase = InitCommand::ask_passphrase()?;
+        match wallet_client.new_wallet(&name.as_str(), &passphrase) {
+            Ok(_a) => {}
+            Err(b) => {
+                println!("new wallet fail={}", b.to_string());
+            }
+        }
+        success(&format!("Wallet created with name: {}", name));
+
+        let address = wallet_client.new_staking_address(&name.as_str(), &passphrase)?;
+        success(&format!("New address: {}", address));
+        self.staking_account_address = address.to_string().trim().to_string();
+        println!("staking address={}", self.staking_account_address);
+        assert!(address.to_string().trim().to_string().len() == 42);
+        Ok(())
+    }
+    pub fn execute(&mut self) -> Result<(), Error> {
+        println!("initialize");
+        self.prepare_tendermint()
+            .and_then(|_| self.read_tendermint_genesis())
+            .and_then(|_| self.read_staking_address())
+            .and_then(|_| self.read_information())
+            .and_then(|_| self.generate_app_info())
+            .and_then(|_| self.write_tendermint_genesis())
+            .map_err(|e| format_err!("init error={}", e))
     }
 
     pub fn storage_path() -> String {
