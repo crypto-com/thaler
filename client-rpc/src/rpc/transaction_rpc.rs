d7fb61a048740ada32f5bd4f2dba27a7e49358ab
Calvin Lau <calvin@mona.co>
2019-08-16 12:09:51 +0800

    🐛 Reverted changes to unspent transactions code

--- client-rpc/src/rpc/transaction_rpc.rs
+++ client-rpc/src/rpc/transaction_rpc.rs
@@ -2,17 +2,24 @@ use failure::ResultExt;
 use hex::{decode, encode};
 use jsonrpc_core::Result;
 use jsonrpc_derive::rpc;
+use serde::{Deserialize, Serialize};
 use secstr::SecUtf8;
 
 use chain_core::tx::TransactionId;
 use chain_core::tx::data::attribute::TxAttributes;
 use chain_core::tx::data::input::{TxoPointer, TxoIndex};
-use chain_core::tx::data::Tx;
+use chain_core::tx::data::{Tx, TxId};
 use chain_core::tx::data::output::TxOut;
 use chain_core::common::{H256, HASH_SIZE_256};
 use client_common::{Error, ErrorKind, PublicKey, Result as CommonResult};
 
 use crate::server::{to_rpc_error, WalletRequest};
+
+#[derive(Serialize, Deserialize)]
+pub struct RawTransaction {
+    tx: Tx,
+    tx_id: TxId,
+}
 
 #[rpc]
 pub trait TransactionRpc: Send + Sync {
@@ -21,8 +28,8 @@ pub trait TransactionRpc: Send + Sync {
         &self,
         inputs: Vec<TxoPointer>,
         outputs: Vec<TxOut>,
-        attributes: TxAttributes,
-    ) -> Result<String>;
+        view_keys: Vec<String>,
+    ) -> Result<RawTransaction>;
 }
 
 pub struct TransactionRpcImpl { }
@@ -34,18 +41,21 @@ impl TransactionRpcImpl {
 }
 
 impl TransactionRpc for TransactionRpcImpl {
-}
+    fn create_raw(
+        &self,
+        inputs: Vec<TxoPointer>,
+        outputs: Vec<TxOut>,
+        view_keys: Vec<String>,
+    ) -> Result<RawTransaction> {
+        let tx = Tx {
+            inputs,
+            outputs,
+            attributes,
+        } 
 
-fn build_raw_transaction(
-    inputs: Vec<TxoPointer>,
-    outputs: Vec<TxOut>,
-    attributes: TxAttributes,
-) -> (Tx, TxId) {
-    let tx = Tx {
-        inputs,
-        outputs,
-        attributes,
-    } 
-
-    (tx, tx.id())
+        RawTransaction {
+            tx,
+            tx.id(),
+        }
+    }
 }
