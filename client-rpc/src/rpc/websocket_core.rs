8e370d3e54d63fc08036026b12db9ef438654c74
Jongwhan Lee <leejw51@gmail.com>
2019-08-17 18:53:54 +0800

    unwrap fix
    display refactoring
    
    
    set timeout as constant

--- client-rpc/src/rpc/websocket_core.rs
+++ client-rpc/src/rpc/websocket_core.rs
@@ -17,7 +17,9 @@ use websocket::result::WebSocketError;
 use websocket::{ClientBuilder, OwnedMessage};
 
 use serde_json::json;
-
+const WAIT_PROCESS_TIME: u128 = 5000; // milli seconds
+const BLOCK_REQUEST_TIME: u128 = 10; // milli seconds
+const RECEIVE_TIMEOUT: u64 = 10; //  milli seconds
 #[derive(Copy, Clone, Debug)]
 pub enum WebsocketState {
     ReadyProcess,
@@ -51,7 +53,7 @@ impl WebsocketCore {
             old_time: SystemTime::now(),
             old_blocktime: SystemTime::now(),
             state_time: SystemTime::now(),
-            current_height: 1,
+            current_height: 0,
             max_height: 0,
             state: WebsocketState::ReadyProcess,
         }
@@ -60,25 +62,31 @@ impl WebsocketCore {
     fn get_latest_height(&self) -> u64 {
         0
     }
-    fn save_block(&mut self, value: &Value) {
+    fn do_save_block(&mut self, value: &Value, kind: &str) {
+        if value.is_null() {
+            return;
+        }
         let block_length = serde_json::to_string(&value).unwrap().len();
-        let block_height = value["result"]["block"]["header"]["height"]
-            .as_str()
-            .unwrap();
+        let block_height = value["header"]["height"].as_str().unwrap();
         let bh = block_height.parse::<u64>().unwrap();
+        if bh != self.current_height + 1 {
+            return;
+        }
+        self.current_height = bh;
 
         let rate = bh as f64 / self.max_height as f64 * 100.0;
-        println!(
-            "save block {:.2}% {}/{}  size={}",
-            rate, block_height, self.max_height, block_length
-        );
 
-        if bh >= self.max_height {
-            self.wait();
+        if self.current_height >= self.max_height {
+            self.max_height = self.current_height;
         }
+
+        println!(
+            "**** {} save block {:.2}% {}/{}  size={}",
+            kind, rate, block_height, self.max_height, block_length
+        );
     }
 
-    pub fn wait(&mut self) {
+    pub fn change_to_wait(&mut self) {
         self.state = WebsocketState::WaitProcess;
         self.state_time = SystemTime::now();
     }
@@ -90,20 +98,28 @@ impl WebsocketCore {
     // received
     fn do_parse(&mut self, value: Value) -> Option<()> {
         let id = value["id"].as_str()?;
-        //println!("do_parse {}", value.to_string());
         match id {
             "subscribe_reply#event" => {
-                let block = value["result"]["data"]["value"]["block"].to_string();
-                println!("receive new block {:?}", block.to_string());
+                let block = value["result"]["data"]["value"]["block"].clone();
+                let height = block["header"]["height"].as_str().unwrap();
+                self.do_save_block(&block, "event");
             }
             "status_reply" => {
                 let height = value["result"]["sync_info"]["latest_block_height"].as_str()?;
-                println!("receive status height={}", height);
                 self.prepare_get_blocks(height.to_string());
             }
             "block_reply" => {
-                let block_height = value["result"]["block"]["header"]["height"].as_str()?;
-                self.save_block(&value);
+                let block = value["result"]["block"].clone();
+                if block.is_null() {
+                    self.change_to_wait();
+                } else {
+                    self.do_save_block(&block, "get block");
+
+                    if self.current_height >= self.max_height {
+                        println!("all synced.. wait");
+                        self.change_to_wait();
+                    }
+                }
             }
             _ => {}
         }
@@ -119,12 +135,20 @@ impl WebsocketCore {
         }
     }
     pub fn prepare_get_blocks(&mut self, height: String) {
-        self.state = WebsocketState::GetBlocks;
         self.max_height = height.parse::<u64>().unwrap();
-        //    self.current_height = 1;
+        if self.current_height < self.max_height {
+            self.state = WebsocketState::GetBlocks;
+
+            println!(
+                "get blocks current {}  max_height {}",
+                self.current_height, self.max_height
+            );
+        } else {
+            self.change_to_wait();
+            println!("synced now");
+        }
     }
     pub fn check_status(&mut self) {
-        println!("check status");
         let mut sink = self.sender.clone().wait();
         sink.send(OwnedMessage::Text(CMD_STATUS.to_string()))
             .unwrap();
@@ -139,7 +163,6 @@ impl WebsocketCore {
             return;
         }
         self.old_time = now;
-        println!("polling diff={} state={:?}", diff, self.state);
         self.polling_state();
     }
 
@@ -152,8 +175,7 @@ impl WebsocketCore {
                 let now = SystemTime::now();
                 let diff = now.duration_since(self.state_time).unwrap().as_millis();
 
-                println!("wait...");
-                if diff > 10000 {
+                if diff > WAIT_PROCESS_TIME {
                     self.state = WebsocketState::ReadyProcess;
                 }
             }
@@ -166,7 +188,7 @@ impl WebsocketCore {
         let now = SystemTime::now();
         let diff = now.duration_since(self.old_blocktime).unwrap().as_millis();
 
-        if diff < 10 {
+        if diff < BLOCK_REQUEST_TIME {
             return;
         }
         self.old_blocktime = now;
@@ -175,18 +197,17 @@ impl WebsocketCore {
 
     pub fn send_request_block(&mut self) {
         let mut json: Value = serde_json::from_str(CMD_BLOCK).unwrap();
-        json["params"] = json!([self.current_height.to_string()]);
-        //println!("{}", json.to_string());
+        let request = self.current_height + 1;
+        json["params"] = json!([request.to_string()]);
         let mut sink = self.sender.clone().wait();
         sink.send(OwnedMessage::Text(json.to_string())).unwrap();
-        self.current_height += 1;
     }
 
     pub fn start(&mut self) {
         loop {
             let _ = self
                 .my_receiver
-                .recv_timeout(time::Duration::from_millis(10))
+                .recv_timeout(time::Duration::from_millis(RECEIVE_TIMEOUT))
                 .map(|a| {
                     self.parse(a);
                 });
