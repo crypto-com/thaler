8a17327141e911f7e56a9c2132cf35a80a3ed379
Calvin Lau <calvin@mona.co>
2019-08-19 21:20:55 +0800

    💚 Fixed tests because of changes in RPC response

--- integration-tests/client-rpc/test/wallet-transaction.test.ts
+++ integration-tests/client-rpc/test/wallet-transaction.test.ts
@@ -235,7 +235,7 @@ describe("Wallet transaction", () => {
 			},
 			"Sender should have one Incoming transaction",
 		);
-		expect(senderWalletLastTransaction.kind).to.eq("incoming");
+		expect(senderWalletLastTransaction.kind).to.eq(TransactionDirection.INCOMING);
 		const senderWalletIncomingAmount = senderWalletLastTransaction.amount;
 		expect(
 			new BigNumber(senderWalletIncomingAmount).isLessThan(
