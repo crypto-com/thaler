d8b77eccf4c7bd064a6bf83e64a17f9b9f4466d3
Tomas Tauber <2410580+tomtau@users.noreply.github.com>
2019-08-22 15:48:08 +0800

    Problem: vulnerabilities in dependencies (CRO-41) (#307)
    Solution:
    - added "cargo audit" to CI
    - RUSTSEC-2019-0011: upgraded dependencies + changed chain-abci to the new starling API
    - RUSTSEC-2018-0006: migrated from clap-yaml to structopt in chain-abci
    this wasn't very critical, as it only applied to macro-level yaml parsing in chain-abci -- as the rest of repo uses structopt, it was planned to migrate to it anyway

--- chain-abci/src/main.rs
+++ chain-abci/src/main.rs
@@ -2,54 +2,90 @@ mod app;
 mod enclave_bridge;
 mod storage;
 
-use clap::load_yaml;
-use clap::App;
 use log::info;
-use std::net::SocketAddr;
+use std::net::{IpAddr, SocketAddr};
 use zmq::{Context, REQ};
 
 use crate::app::ChainNodeApp;
 use crate::enclave_bridge::ZmqEnclaveClient;
 use crate::storage::*;
 use chain_core::init::network::{get_network, get_network_id, init_chain_id};
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "chain-abci",
+    about = " Pre-alpha version prototype of Crypto.com Chain node (Tendermint ABCI application)."
+)]
+struct AbciOpt {
+    #[structopt(
+        short = "d",
+        long = "data",
+        default_value = ".cro-storage/",
+        help = "Sets a data storage directory"
+    )]
+    data: String,
+    #[structopt(
+        short = "p",
+        long = "port",
+        default_value = "26658",
+        help = "Sets a port to listen on"
+    )]
+    port: u16,
+    #[structopt(
+        short = "h",
+        long = "host",
+        default_value = "127.0.0.1",
+        help = "Sets the ip address to listen on"
+    )]
+    host: IpAddr,
+    #[structopt(
+        short = "g",
+        long = "genesis_app_hash",
+        help = "The expected app hash after init chain (computed from the merkle trie root etc.)"
+    )]
+    genesis_app_hash: String,
+    #[structopt(
+        short = "c",
+        long = "chain_id",
+        help = "The expected chain id from init chain (the name convention is \"...some-name...-<TWO_HEX_DIGITS>\")"
+    )]
+    chain_id: String,
+    #[structopt(
+        short = "e",
+        long = "enclave_server",
+        help = "Connection string (e.g. ipc://enclave.socket or tcp://127.0.0.1:25933) for ZeroMQ server wrapper around the transaction validation enclave."
+    )]
+    enclave_server: String,
+}
 
 fn main() {
-    // TODO
     env_logger::init();
-    let yaml = load_yaml!("../cli.yml");
-    let matches = App::from_yaml(yaml).get_matches();
+    let opt = AbciOpt::from_args();
     let ctx = Context::new();
     let socket = ctx.socket(REQ).expect("failed to init zmq context");
-    let enclave_conn = matches
-        .value_of("enclave_server")
-        .unwrap_or("tcp://127.0.0.1:25933");
     socket
-        .connect(enclave_conn)
+        .connect(&opt.enclave_server)
         .expect("failed to connect to enclave zmq wrapper");
     let proxy = ZmqEnclaveClient::new(socket);
-    let data = matches.value_of("data").unwrap_or(".cro-storage/");
-    let port = matches.value_of("port").unwrap_or("26658");
-    let host = matches.value_of("host").unwrap_or("127.0.0.1");
-    let genesis_app_hash = matches.value_of("genesis_app_hash").unwrap();
-    let chain_id = matches.value_of("chain_id").unwrap();
 
-    init_chain_id(chain_id);
+    init_chain_id(&opt.chain_id);
     info!(
         "network={:?} network_id={:X}",
         get_network(),
         get_network_id()
     );
 
-    let addr = SocketAddr::new(host.parse().unwrap(), port.parse().unwrap());
+    let addr = SocketAddr::new(opt.host, opt.port);
     info!("starting up");
     abci::run(
         addr,
         ChainNodeApp::new(
             proxy,
-            &genesis_app_hash,
-            &chain_id,
-            &StorageConfig::new(data, StorageType::Node),
-            &StorageConfig::new(data, StorageType::AccountTrie),
+            &opt.genesis_app_hash,
+            &opt.chain_id,
+            &StorageConfig::new(&opt.data, StorageType::Node),
+            &StorageConfig::new(&opt.data, StorageType::AccountTrie),
         ),
     );
 }
