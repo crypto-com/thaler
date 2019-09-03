93c9cbf684f1ab884d1dc8639b3ffba10e782f98
Jongwhan Lee <jonghwan@crypto.com>
2019-09-03 16:01:58 +0800

    change doc style

--- client-index/src/auto_synchronizer.rs
+++ client-index/src/auto_synchronizer.rs
@@ -1,15 +1,15 @@
-/**
-automatic sync
+//!
+//! automatic sync
+//!
+//! how to use
+//! 1. create auto-sync & run
+//! let autosync: AutoSync::new();
+//! autosync.run(url, tendermint_client, storage.clone(), block_handler);
+//!
+//! 2. unlock wallet
+//! autosync.add_wallet(request.name, view_key, private_key, staking_addresses);
+//!
 
-how to use
-1. create auto-sync & run
-let autosync: AutoSync::new();
-autosync.run(url, tendermint_client, storage.clone(), block_handler);
-
-2. unlock wallet
-autosync.add_wallet(request.name, view_key, private_key, staking_addresses);
-
-*/
 use crate::service::GlobalStateService;
 use crate::BlockHandler;
 use chain_core::state::account::StakedStateAddress;
