5e2c835c7fbf79e5f230d567c462ea35ac399b9d
Tomas Tauber <2410580+tomtau@users.noreply.github.com>
2019-11-25 10:56:04 +0800

    Problem: drone pipelines not portable and reproducible (CRO-598)
    Solution: created a simple Docker image uploaded to the Docker hub and updated the drone pipelines to use it

---
+++ ci-scripts/tx-query-hw-test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+source /root/.docker_bashrc
+
+export SGX_MODE=HW
+export NETWORK_ID=ab
+export RUST_LOG=debug
+export RUST_BACKTRACE=1
+export RUSTFLAGS=-Ctarget-feature=+aes,+sse2,+sse4.1,+ssse3
+
+ls /dev/sgx
+
+LD_LIBRARY_PATH=/opt/intel/libsgx-enclave-common/aesm /opt/intel/libsgx-enclave-common/aesm/aesm_service &
+
+echo "[aesm_service] Running in background ..."
+# Wait for aesm_service to initialize
+sleep 1
+
+
+cd /chain/chain-tx-enclave/tx-validation
+make clean
+make
+
+export SGX_TEST=1
+cd /chain/chain-tx-enclave/tx-query
+make clean
+make
+
+export TX_VALIDATION_BIN_DIR=/chain/chain-tx-enclave/tx-validation/bin/
+export TX_QUERY_APP_PORT=`/chain/ci-scripts/find-free-port.sh`
+cd bin
+# assumes SPID + IAS_API_KEY environment variables are set from outside / docker
+./tx-query-app
