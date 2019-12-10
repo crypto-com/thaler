a578999a0453edf84f7d312cd0d09bcb06004124
Calvin Lau <calvinlauco@gmail.com>
2019-12-10 19:52:34 +0800

    :sparkles: Add required_fee and fee method

--- client-core/src/transaction_builder/raw_transfer_transaction_builder.rs
+++ client-core/src/transaction_builder/raw_transfer_transaction_builder.rs
@@ -1,4 +1,5 @@
 //! Builder for building raw transfer transaction
+use std::ops::Sub;
 use std::slice::Iter;
 
 use parity_scale_codec::{Decode, Encode};
@@ -93,7 +94,8 @@ where
             prev_tx_out: input.1,
             witness: None,
         });
-        self.clear_witness()
+
+        self.clear_witness();
     }
 
     /// Append output to raw transaction
@@ -210,10 +212,36 @@ where
         Ok(&mut self.raw_transaction.inputs[index])
     }
 
-    /// Returns fee of completed transfer transaction
+    /// Return the fee paid in this transaction
     /// # Error
     /// Returns error when transaction is incompleted
     pub fn fee(&self) -> Result<Coin> {
+        if !self.is_completed() {
+            return Err(Error::new(
+                ErrorKind::InvalidInput,
+                "Missing signature in inputs",
+            ));
+        }
+
+        self.verify_output_does_not_exceed_input_amount()?;
+
+        let input_value = self.total_input_amount()?;
+        let output_value = self.total_output_amount()?;
+
+        Ok(input_value.sub(output_value).unwrap())
+    }
+
+    /// Returns required fee of completed transfer transaction according to fee
+    /// algorithm
+    /// # Error
+    /// Returns error when transaction is incompleted
+    pub fn required_fee(&self) -> Result<Coin> {
+        if !self.is_completed() {
+            return Err(Error::new(
+                ErrorKind::InvalidInput,
+                "Missing signature in inputs",
+            ));
+        }
         let fee = self
             .fee_algorithm
             .calculate_for_txaux(&self.to_tx_aux()?)
@@ -319,20 +347,9 @@ where
     }
 
     fn verify_output_does_not_exceed_input_amount(&self) -> Result<()> {
-        let input_value = sum_coins(self.iter_inputs().map(|input| input.prev_tx_out.value))
-            .chain(|| {
-                (
-                    ErrorKind::VerifyError,
-                    "Sum of input values exceeds maximum allowed amount",
-                )
-            })?;
-        let output_value =
-            sum_coins(self.iter_outputs().map(|output| output.value)).chain(|| {
-                (
-                    ErrorKind::VerifyError,
-                    "Sum of output values exceeds maximum allowed amount",
-                )
-            })?;
+        let input_value = self.total_input_amount()?;
+        let output_value = self.total_output_amount()?;
+
         // TODO: Include fee in calculation?
         if input_value < output_value {
             return Err(Error::new(
@@ -361,6 +378,27 @@ where
         }
 
         Ok(())
+    }
+
+    /// Returns the total amount of all inputs
+    pub fn total_input_amount(&self) -> Result<Coin> {
+        sum_coins(self.iter_inputs().map(|input| input.prev_tx_out.value))
+            .chain(|| {
+                (
+                    ErrorKind::VerifyError,
+                    "Sum of input values exceeds maximum allowed amount",
+                )
+            })
+    }
+
+    /// Returns the total amount of all outputs
+    pub fn total_output_amount(&self) -> Result<Coin> {
+        sum_coins(self.iter_outputs().map(|output| output.value)).chain(|| {
+            (
+                ErrorKind::VerifyError,
+                "Sum of output values exceeds maximum allowed amount",
+            )
+        })
     }
 
     /// Returns if the all inputs of the transaction are signed
@@ -909,7 +947,7 @@ mod raw_transfer_transaction_builder_tests {
         use super::*;
 
         #[test]
-        fn fee_should_return_error_when_raw_transaction_is_incompleted() {
+        fn should_return_error_when_raw_transaction_is_incompleted() {
             let (_, _, transfer_addr) = create_key_pair_and_transfer_addr();
             let builder = create_2in2out_testing_raw_transaction_builder(transfer_addr);
 
@@ -919,7 +957,62 @@ mod raw_transfer_transaction_builder_tests {
         }
 
         #[test]
-        fn estimate_fee_should_be_greater_than_or_equal_to_fee() {
+        fn should_return_difference_between_inputs_and_outputs() {
+            let (private_key, public_key, transfer_addr) = create_key_pair_and_transfer_addr();
+
+            let attributes = TxAttributes::default();
+            let fee_algorithm = create_testing_fee_algorithm();
+            let tx_obfuscation = MockTransactionCipher;
+            let mut builder =
+                RawTransferTransactionBuilder::new(attributes, fee_algorithm, tx_obfuscation);
+
+            builder.add_input((
+                TxoPointer::new(random(), 0),
+                TxOut::new(transfer_addr.clone(), Coin::new(100).unwrap()),
+            ));
+            builder.add_input((
+                TxoPointer::new(random(), 0),
+                TxOut::new(transfer_addr, Coin::new(200).unwrap()),
+            ));
+
+            builder.add_output(TxOut::new(
+                ExtendedAddr::OrTree(random()),
+                Coin::new(50).unwrap(),
+            ));
+            builder.add_output(TxOut::new(
+                ExtendedAddr::OrTree(random()),
+                Coin::new(100).unwrap(),
+            ));
+
+            let witness = create_public_key_witness(private_key, public_key, builder.tx_id());
+            builder
+                .add_witness(0, witness.clone())
+                .expect("should add witness to input index");
+            builder
+                .add_witness(1, witness)
+                .expect("should add witness to input index");
+
+            let fee = builder.fee().unwrap();
+
+            assert_eq!(fee, Coin::new(150).unwrap());
+        }
+    }
+
+    mod required_fee {
+        use super::*;
+
+        #[test]
+        fn should_return_error_when_raw_transaction_is_incompleted() {
+            let (_, _, transfer_addr) = create_key_pair_and_transfer_addr();
+            let builder = create_2in2out_testing_raw_transaction_builder(transfer_addr);
+
+            let err = builder.required_fee().unwrap_err();
+            assert_eq!(err.kind(), ErrorKind::VerifyError);
+            assert_eq!(err.message(), "Missing signature in inputs");
+        }
+
+        #[test]
+        fn estimate_fee_should_be_greater_than_or_equal_to_required_fee() {
             let (private_key, public_key, transfer_addr) = create_key_pair_and_transfer_addr();
             let mut builder = create_2in2out_testing_raw_transaction_builder(transfer_addr);
             let witness = create_public_key_witness(private_key, public_key, builder.tx_id());
@@ -930,10 +1023,10 @@ mod raw_transfer_transaction_builder_tests {
                 .add_witness(1, witness)
                 .expect("should add witness to input index");
 
-            let fee = builder.fee().unwrap();
+            let required_fee = builder.required_fee().unwrap();
             let estimated_fee = builder.estimate_fee().unwrap();
 
-            assert!(estimated_fee >= fee);
+            assert!(estimated_fee >= required_fee);
         }
     }
 
