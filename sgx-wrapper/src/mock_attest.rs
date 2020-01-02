b2eb4964c70c1b43e650d9228d93e64d79e608b4
yihuang <yi.codeplayer@gmail.com>
2020-01-02 15:13:23 +0800

    Builtin mock certificates for mock attested channel

--- sgx-wrapper/src/mock_attest.rs
+++ sgx-wrapper/src/mock_attest.rs
@@ -1,43 +1,23 @@
 pub use rustls;
-use std::env;
-use std::fs;
-use std::io::BufReader;
 use std::sync::Arc;
 
-fn load_certs(filename: &str) -> Vec<rustls::Certificate> {
-    let certfile = fs::File::open(filename).expect("cannot open certificate file");
-    let mut reader = BufReader::new(certfile);
-    rustls::internal::pemfile::certs(&mut reader).unwrap()
+const MOCK_CERT_FILE: &[u8] = include_bytes!("../certs/selfsigned.crt");
+const MOCK_KEY_FILE: &[u8] = include_bytes!("../certs/private.key");
+
+fn load_certs() -> Vec<rustls::Certificate> {
+    rustls::internal::pemfile::certs(&mut std::io::Cursor::new(MOCK_CERT_FILE.to_vec())).unwrap()
 }
 
-fn load_private_key(filename: &str) -> rustls::PrivateKey {
-    let rsa_keys = {
-        let keyfile = fs::File::open(filename).expect("cannot open private key file");
-        let mut reader = BufReader::new(keyfile);
-        rustls::internal::pemfile::rsa_private_keys(&mut reader)
-            .expect("file contains invalid rsa private key")
-    };
-
-    let pkcs8_keys = {
-        let keyfile = fs::File::open(filename).expect("cannot open private key file");
-        let mut reader = BufReader::new(keyfile);
-        rustls::internal::pemfile::pkcs8_private_keys(&mut reader)
-            .expect("file contains invalid pkcs8 private key (encrypted keys not supported)")
-    };
-
-    // prefer to load pkcs8 keys
-    if !pkcs8_keys.is_empty() {
-        pkcs8_keys[0].clone()
-    } else {
-        assert!(!rsa_keys.is_empty());
-        rsa_keys[0].clone()
-    }
+fn load_private_key() -> rustls::PrivateKey {
+    rustls::internal::pemfile::rsa_private_keys(&mut std::io::Cursor::new(MOCK_KEY_FILE.to_vec()))
+        .expect("file contains invalid rsa private key")[0]
+        .clone()
 }
 
 pub fn get_tls_config() -> Arc<rustls::ServerConfig> {
     let mut config = rustls::ServerConfig::new(rustls::NoClientAuth::new());
-    let certs = load_certs(&env::var("MOCK_TLS_CERTS").expect("MOCK_TLS_CERTS missing"));
-    let privkey = load_private_key(&env::var("MOCK_TLS_KEY").expect("MOCK_TLS_KEY missing"));
+    let certs = load_certs();
+    let privkey = load_private_key();
     config
         .set_single_cert_with_ocsp_and_sct(certs, privkey, vec![], vec![])
         .expect("bad certificates/private key");
