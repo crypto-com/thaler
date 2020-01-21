3651fb82f6e6444a562ae24d8b763d5004789332
yihuang <yi.codeplayer@gmail.com>
2020-01-21 11:38:04 +0800

    Problem (Fix #851): no unbonding validator test
    Solution:
    - integrate unbond test into join test.
    - use tx_search to wait for transaction (Fix #948)
    - change node stopping sequence, try to fix app_hash mismatch problem

--- integration-tests/multinode/join_test.py
+++ integration-tests/multinode/join_test.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 import os
-import time
 from chainrpc import RPC
 from chainbot import SigningKey
-from common import wait_for_pending, wait_for_validators, wait_for_blocks
+from common import wait_for_tx, wait_for_validators, wait_for_blocks, wait_for_blocktime
 
 '''
-three node, node0 has zero bonded coin.
+three node, equal bonded_coin.
 
 procedure:
-- withdraw_all_unbonded from second staking address to first transfer address
-- deposit above utxo into first staking address
-- call join
+- wait for 3 validators online
+- unbond part of coin from node0, check validator count not change.
+- unbond all of the coins, check validator count decrease
+- withdraw and deposit all the coins
+- send join request, check validator count increase
 '''
 
 BASE_PORT = int(os.environ.get('BASE_PORT', 25560))
@@ -21,30 +22,39 @@ VALIDATOR_SEED = '9f06b6bb885d0143092aad7a6df2f1cae7690fcbfa1edb8727542372085ade
 
 rpc = RPC(BASE_PORT)
 
-print('Wait for 2 validators online')
-wait_for_validators(rpc, 2)
+print('Wait for 3 validators online')
+wait_for_validators(rpc, 3)
 
 enckey = rpc.wallet.enckey()
-bonded_staking, unbonded_staking = rpc.address.list(enckey=enckey)[:2]
+bonded_staking, _ = rpc.address.list(enckey=enckey)[:2]
 
-print('Withdraw unbonded', unbonded_staking)
+rpc.staking.unbond(bonded_staking, 100000000000000000, enckey=enckey)
+print('Wait for 3 blocks')
+wait_for_blocks(rpc, 3)
+rpc.wallet.sync(enckey=enckey)
+
+assert len(rpc.chain.validators()['validators']) == 2
+
+unbonded_from = rpc.staking.state(bonded_staking, enckey=enckey)['unbonded_from']
+print('Wait until unbonded_from', unbonded_from)
+wait_for_blocktime(rpc, unbonded_from)
+
+print('Withdraw unbonded', bonded_staking)
 transfer = rpc.address.list(enckey=enckey, type='transfer')[0]
+txid = rpc.staking.withdraw_all_unbonded(bonded_staking, transfer, enckey=enckey)
 
-# FIXME status rpc call deserialization fail when block height = 0.
-time.sleep(10)
-txid = rpc.staking.withdraw_all_unbonded(unbonded_staking, transfer, enckey=enckey)
-
-print('Wait for wallet to sync...')
-wait_for_pending(rpc, enckey)
+print('Wait for transaction', txid)
+wait_for_tx(rpc, txid)
+rpc.wallet.sync(enckey=enckey)
 
 print('Balance', rpc.wallet.balance(enckey=enckey))
 print('UtxO', rpc.wallet.utxo(enckey=enckey))
 
 print('Deposit bonded', txid, bonded_staking)
-rpc.staking.deposit(bonded_staking, [{'id': txid, 'index': 0}], enckey=enckey)
+txid = rpc.staking.deposit(bonded_staking, [{'id': txid, 'index': 0}], enckey=enckey)
 
-print('Wait for wallet to sync...')
-wait_for_blocks(rpc, 3)
+print('Wait for transaction', txid)
+wait_for_tx(rpc, txid)
 rpc.wallet.sync(enckey=enckey)
 
 print('Bonded state:', rpc.staking.state(bonded_staking, enckey=enckey))
@@ -57,9 +67,10 @@ txid = rpc.staking.join(
     enckey=enckey
 )
 
-print('Wait for 3 blocks')
-wait_for_blocks(rpc, 3)
-rpc.wallet.sync(enckey=enckey)
+print('Wait for transaction', txid)
+wait_for_tx(rpc, txid)
 
-print('validators', len(rpc.chain.validators()['validators']))
+print('Wait for validator set to take effect')
+wait_for_blocks(rpc, 3)
+
 assert len(rpc.chain.validators()['validators']) == 3
