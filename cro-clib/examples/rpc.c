6ca75515b0660c829bde2a9b6e0a7802f715bd25
jongwhan lee <jonghwan@crypto.com>
2020-03-04 14:55:42 +0800

    Problem: repeated db open/close and thread spawning `jsonrpc_call` C binding#1132
    Solution: add context, divide into 3 functions
    add comments

--- cro-clib/examples/rpc.c
+++ cro-clib/examples/rpc.c
@@ -45,11 +45,38 @@ void sync()
         printf("error: %s\n", buf);
     }
 }
+void context_sync()
+{
+    const int BUFSIZE=1000;
+    char buf[BUFSIZE];
+    char* name= getenv("CRO_NAME");
+    char* passphrase=getenv("CRO_PASSPHRASE");
+    char* enckey=getenv("CRO_ENCKEY");
+    const char* req_template = "{\"jsonrpc\": \"2.0\", \"method\": \"sync\", \"params\": [{\"name\":\"%s\", \"passphrase\":\"%s\",\"enckey\":\"%s\"}], \"id\": 1}";
+    char req[BUFSIZE];
+    sprintf(req, req_template, name, passphrase, enckey);
+
+    const char* wallet_req = "{\"jsonrpc\": \"2.0\", \"method\": \"wallet_list\", \"params\": [], \"id\": 1}";
+
+    printf("sync with context\n");
+    CroJsonRpcPtr rpc= NULL;
+    cro_create_jsonrpc(&rpc, ".storage", "ws://localhost:26657/websocket", 0xab, &progress);
+    cro_run_jsonrpc(rpc, wallet_req, buf, sizeof(buf));
+    printf("response: %s\n", buf);    
+
+    cro_run_jsonrpc(rpc, req, buf, sizeof(buf));
+    printf("response: %s\n", buf);
+    
+    cro_destroy_jsonrpc(rpc);
+    printf("OK");
+
+}
 int test_rpc()
 {    
     printf("test rpc\n");
-    show_wallets();
-    sync();
+    //show_wallets();
+    //sync();
+    context_sync();
     return 0;
 }
 
