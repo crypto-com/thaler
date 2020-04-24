cb669cc670aab1a14d0964bb78f2c9cf4bbc821e
yihuang <huang@crypto.com>
2020-04-24 10:56:53 +0800

    Problem (Fix #1479): mock-utils crash when provided with invalid tx payload
    Solution:
    - Change unwrap to Result

--- chain-tx-enclave/mock-utils/src/lib.rs
+++ chain-tx-enclave/mock-utils/src/lib.rs
@@ -2,13 +2,14 @@ use parity_scale_codec::{Decode, Encode};
 
 use chain_core::state::tendermint::BlockHeight;
 use chain_core::tx::{data::TxId, PlainTxAux, TxObfuscated, TxWithOutputs};
+use chain_tx_validation::Error;
 
 const ENCRYPTION_KEY: u8 = 0x0f;
 const SEAL_KEY: u8 = 0xf0;
-pub fn decrypt(payload: &TxObfuscated) -> PlainTxAux {
+pub fn decrypt(payload: &TxObfuscated) -> Result<PlainTxAux, Error> {
     let unpad = unpad_payload(&payload.txpayload);
     let bs = unpad.iter().map(|b| b ^ ENCRYPTION_KEY).collect::<Vec<_>>();
-    PlainTxAux::decode(&mut bs.as_slice()).unwrap()
+    PlainTxAux::decode(&mut bs.as_slice()).map_err(|_| Error::EnclaveRejected)
 }
 
 fn unpad_payload(payload: &[u8]) -> &[u8] {
@@ -21,9 +22,9 @@ pub fn seal(tx: &TxWithOutputs) -> Vec<u8> {
         .map(|b| b ^ SEAL_KEY)
         .collect::<Vec<_>>()
 }
-pub fn unseal(payload: &[u8]) -> TxWithOutputs {
+pub fn unseal(payload: &[u8]) -> Result<TxWithOutputs, Error> {
     let bytes = payload.iter().map(|b| b ^ SEAL_KEY).collect::<Vec<_>>();
-    TxWithOutputs::decode(&mut bytes.as_slice()).unwrap()
+    TxWithOutputs::decode(&mut bytes.as_slice()).map_err(|_| Error::EnclaveRejected)
 }
 fn pad_payload(payload: &[u8]) -> Vec<u8> {
     let mut result = Vec::with_capacity(payload.len() + 16);
