dc636eb5d87762fa78fad3eb9a40d4159a5a002e
Tomas Tauber <2410580+tomtau@users.noreply.github.com>
2020-05-18 11:50:58 +0800

    Problem: lvi hardening not enabled
    Solution: updated Makefile + build.rs flags
    Note that this covers the Intel SDK-related C code.
    For Rust code, there's a pending PR to update LLVM
    https://github.com/rust-lang/llvm-project/pull/58/commits/88008b8cd7b5e165a830f33b7d946b8bd3ec2a51#diff-c9b310226bd6dbea006b9c172ffed354R2331

--- chain-tx-enclave/tx-validation/enclave/build.rs
+++ chain-tx-enclave/tx-validation/enclave/build.rs
@@ -22,18 +22,51 @@ fn main() {
         ])
         .status()
         .unwrap();
+    let mut build = cc::Build::new();
 
-    cc::Build::new()
+    build
         .file("Enclave_t.c")
         .include("../../rust-sgx-sdk/common/inc")
         .include("../../rust-sgx-sdk/edl")
         .include(&format!("{}/include", sdk_dir))
         .include(&format!("{}/include/tlibc", sdk_dir))
-        .flag("-nostdinc")
-        .flag("-fvisibility=hidden")
-        .flag("-fpie")
+        .opt_level(2)
         .flag("-fstack-protector")
-        .compile("enclave.a");
+        .flag("-ffreestanding")
+        .flag("-fpie")
+        .flag("-fno-strict-overflow")
+        .flag("-fno-delete-null-pointer-checks")
+        .flag("-fvisibility=hidden");
+
+    let mitigation_cflags1 = "-mindirect-branch-register";
+    let mitigation_cflags2 = "-mfunction-return=thunk-extern";
+    let mitigation_asflags = "-fno-plt";
+    let mitigation_loadflags1 = "-Wa,-mlfence-after-load=yes";
+    let mitigation_loadflags2 = "-Wa,-mlfence-before-ret=not";
+    let mitigation_cfflags1 = "-Wa,-mlfence-before-indirect-branch=register";
+    let mitigation_cfflags2 = "-Wa,-mlfence-before-ret=not";
+    let mitigation = env::var("MITIGATION_CVE_2020_0551").unwrap_or("LOAD".to_owned());
+    match mitigation.as_ref() {
+        "LOAD" => {
+            build
+                .flag(mitigation_cflags1)
+                .flag(mitigation_cflags2)
+                .flag(mitigation_asflags)
+                .flag(mitigation_loadflags1)
+                .flag(mitigation_loadflags2);
+        }
+        "CF" => {
+            build
+                .flag(mitigation_cflags1)
+                .flag(mitigation_cflags2)
+                .flag(mitigation_asflags)
+                .flag(mitigation_cfflags1)
+                .flag(mitigation_cfflags2);
+        }
+        _ => {}
+    }
+
+    build.compile("enclave.a");
 
     println!("cargo:rerun-if-changed=Enclave.edl");
 }
