981d4217d4a51b167ce33a7c82e4ffa78679f19a
Calvin Lau <calvinlauco@gmail.com>
2020-07-01 01:40:53 +0800

    Problem: (Fix #1849) Makefile run chain-abci with tx_query set to docker hostname
    Solution: Provide an option to customize the hostname

--- ci-scripts/run_chain_abci.sh
+++ ci-scripts/run_chain_abci.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -e
 
+if [[ ! -z "$TX_QUERY_HOSTNAME" ]]; then
+  PREFIX=""
+fi
+TX_QUERY_HOSTNAME=${TX_QUERY_HOSTNAME:-"sgx-query-next"}
+
 echo "[Config] SGX_MODE=${SGX_MODE}"
 echo "[Config] NETWORK_ID=${NETWORK_ID}"
+echo "[Config] TX_QUERY_HOSTNAME=${TX_QUERY_HOSTNAME}"
 
 if [ x"${SGX_MODE}" == "xHW" ]; then
   mkdir -p /var/run/aesmd/
@@ -22,4 +28,4 @@ chain-abci \
     --genesis_app_hash ${APP_HASH} \
     --host 0.0.0.0 \
     --port 26658 \
-    --tx_query ${PREFIX}sgx-query-next:26651
+    --tx_query ${PREFIX}${TX_QUERY_HOSTNAME}:26651
