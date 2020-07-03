264baba6701ccae8eb2e95f722708f31f3783893
Tomas Tauber <2410580+tomtau@users.noreply.github.com>
2020-07-03 16:49:52 +0800

    Problem: fuzzer not being uploaded (fixes #1879)
    Solution: updated the path to the fuzzer binary
    (as cargo fuzz now runs/builds in the release mode?)

--- ci-scripts/fuzzit.sh
+++ ci-scripts/fuzzit.sh
@@ -6,4 +6,4 @@ cargo install cargo-fuzz
 cargo fuzz run abci-cycle -- -runs=0
 wget -q -O fuzzit https://github.com/fuzzitdev/fuzzit/releases/download/v2.4.77/fuzzit_Linux_x86_64
 chmod a+x fuzzit
-./fuzzit create job --type fuzzing abci-cycle ./fuzz/target/x86_64-unknown-linux-gnu/debug/abci-cycle
+./fuzzit create job --type fuzzing abci-cycle ./fuzz/target/x86_64-unknown-linux-gnu/release/abci-cycle
