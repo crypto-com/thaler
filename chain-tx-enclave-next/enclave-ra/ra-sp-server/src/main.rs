df02ef44e2a9975687c97ae5a59800c730fe6a88
Tomas Tauber <2410580+tomtau@users.noreply.github.com>
2020-07-13 09:03:37 +0800

    Problem: Cargo.lock still uses the old format (fixes #1927)
    Solution: re-generated Cargo.lock
    + fixed hpke version until
    https://github.com/crypto-com/chain/issues/1932
    is resolved
    
    + updated ra-sp-server to retry when waiting for aesm

--- chain-tx-enclave-next/enclave-ra/ra-sp-server/src/main.rs
+++ chain-tx-enclave-next/enclave-ra/ra-sp-server/src/main.rs
@@ -11,7 +11,16 @@ fn main() {
     let config = SpRaConfig::from_args();
     env_logger::init();
 
-    let address = config.address.clone();
-    let server = SpRaServer::new(config).unwrap();
-    server.run(address).unwrap();
+    let mut attempt = 0;
+    while attempt < 3 {
+        let config = SpRaConfig::from_args();
+        let address = config.address.clone();
+        if let Ok(server) = SpRaServer::new(config) {
+            server.run(address).unwrap();
+        } else {
+            attempt += 1;
+            std::thread::sleep(std::time::Duration::from_millis(10000));
+        }
+    }
+    panic!("failed to init SpRaServer");
 }
