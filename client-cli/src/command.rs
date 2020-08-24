Bhatv79/chain

Problem: (issue-2135) client-* doesn't know the wallet type from storage
solution:
* add harware wallet type in the storage when create a new wallet
* remove --hardware in client-cli transaction

client-cli/src/command.rs
@@ -45,7 +45,7 @@ use chain_core::tx::fee::LinearFee;
 use client_core::hd_wallet::HardwareKind;
 #[cfg(feature = "mock-hardware-wallet")]
 use client_core::service::MockHardwareService;
-use client_core::service::{HwKeyService, LedgerService};
+use client_core::service::{HwKeyService, LedgerService, WalletService};
 use once_cell::sync::Lazy;
 use std::env;
 
@@ -150,14 +150,6 @@ pub enum Command {
     Transaction {
         #[structopt(subcommand)]
         transaction_command: TransactionCommand,
-        #[structopt(
-            name = "hardware wallet type",
-            long = "hardware",
-            help = "Hardware wallet type",
-            possible_values = &HARDWARE_WALLET_KIND,
-            case_insensitive = false
-        )]
-        hardware: Option<HardwareKind>,
     },
     #[structopt(name = "state", about = "Get staked state of an address")]
     StakedState {
@@ -336,19 +328,22 @@ impl Command {
             }
             Command::Transaction {
                 transaction_command,
-                hardware,
             } => {
                 let storage = SledStorage::new(storage_path())?;
                 let tendermint_client = WebsocketRpcClient::new(&tendermint_url())?;
-                let hw_key_service = match hardware {
-                    None => HwKeyService::default(),
+                let wallet_name = transaction_command.wallet_name();
+                let wallet_service = WalletService::new(storage.clone());
+                let enckey = ask_seckey(None)?;
+                let wallet = wallet_service.get_wallet(&wallet_name, &enckey)?;
+                let hw_key_service = match wallet.hardware_kind {
                     #[cfg(feature = "mock-hardware-wallet")]
-                    Some(HardwareKind::Mock) => HwKeyService::Mock(MockHardwareService::new()),
-                    Some(HardwareKind::Trezor) => HwKeyService::default(),
-                    Some(HardwareKind::Ledger) => {
+                    HardwareKind::Mock => HwKeyService::Mock(MockHardwareService::new()),
+                    HardwareKind::Trezor => HwKeyService::default(),
+                    HardwareKind::Ledger => {
                         let ledger_service = LedgerService::new(true)?;
                         HwKeyService::Ledger(ledger_service)
                     }
+                    HardwareKind::None => HwKeyService::default(),
                 };
                 let signer_manager =
                     WalletSignerManager::new(storage.clone(), hw_key_service.clone());
@@ -374,8 +369,11 @@ impl Command {
                     fee_algorithm,
                     transaction_obfuscation,
                 );
-                transaction_command
-                    .execute(network_ops_client.get_wallet_client(), &network_ops_client)
+                transaction_command.execute(
+                    network_ops_client.get_wallet_client(),
+                    &network_ops_client,
+                    enckey,
+                )
             }
             Command::StakedState {
                 name,
@@ -391,6 +389,7 @@ impl Command {
                         let ledger_service = LedgerService::new(true)?;
                         HwKeyService::Ledger(ledger_service)
                     }
+                    Some(HardwareKind::None) => HwKeyService::default(),
                 };
                 let storage = SledStorage::new(storage_path())?;
                 let tendermint_client = WebsocketRpcClient::new(&tendermint_url())?;

client-cli/src/command/wallet_command.rs
@@ -6,7 +6,8 @@ use client_common::{Error, ErrorKind, PrivateKey, Result, ResultExt};
 use client_core::types::WalletKind;
 use client_core::{Mnemonic, WalletClient};
 
-use crate::{ask_passphrase, ask_seckey};
+use crate::{ask_hardware_kind, ask_passphrase, ask_seckey};
+use client_core::hd_wallet::HardwareKind;
 use client_core::service::WalletInfo;
 use client_core::wallet::WalletRequest;
 use std::fs::File;
@@ -162,8 +163,20 @@ impl WalletCommand {
                 "Passphrases do not match",
             ));
         }
-        let (enckey, mnemonic) =
-            wallet_client.new_wallet(name, &passphrase, wallet_kind, Some(mnemonics_word_count))?;
+
+        let hardware_kind = if wallet_kind == WalletKind::HW {
+            ask_hardware_kind(None)?
+        } else {
+            HardwareKind::None
+        };
+
+        let (enckey, mnemonic) = wallet_client.new_wallet(
+            name,
+            &passphrase,
+            wallet_kind,
+            hardware_kind,
+            Some(mnemonics_word_count),
+        )?;
 
         if let WalletKind::HD = wallet_kind {
             ask("Please store following mnemonic safely to restore your wallet later: ");

client-core/src/signer/wallet_signer.rs
@@ -204,6 +204,7 @@ mod wallet_signer_tests {
     use chain_tx_validation::witness::verify_tx_address;
     use client_common::storage::MemoryStorage;
 
+    use crate::hd_wallet::HardwareKind;
     use crate::types::WalletKind;
     use crate::wallet::{DefaultWalletClient, WalletClient};
 
@@ -218,7 +219,13 @@ mod wallet_signer_tests {
         let wallet_client = DefaultWalletClient::new_read_only(storage.clone());
 
         let (enckey, _) = wallet_client
-            .new_wallet(name, &passphrase, WalletKind::Basic, None)
+            .new_wallet(
+                name,
+                &passphrase,
+                WalletKind::Basic,
+                HardwareKind::None,
+                None,
+            )
             .unwrap();
 
         let public_keys = vec![
@@ -259,7 +266,13 @@ mod wallet_signer_tests {
         let wallet_client = DefaultWalletClient::new_read_only(storage.clone());
 
         let (enckey, _) = wallet_client
-            .new_wallet(name, &passphrase, WalletKind::Basic, None)
+            .new_wallet(
+                name,
+                &passphrase,
+                WalletKind::Basic,
+                HardwareKind::None,
+                None,
+            )
             .unwrap();
 
         let public_keys = vec![

client-core/src/transaction_builder/default_wallet_transaction_builder.rs
@@ -239,6 +239,7 @@ mod default_wallet_transaction_builder_tests {
     use client_common::storage::MemoryStorage;
     use client_common::Transaction;
 
+    use crate::hd_wallet::HardwareKind;
     use crate::service::HwKeyService;
     use crate::signer::WalletSignerManager;
     use crate::types::WalletKind;
@@ -287,7 +288,13 @@ mod default_wallet_transaction_builder_tests {
         let wallet_client = DefaultWalletClient::new_read_only(storage.clone());
 
         let (enckey, _) = wallet_client
-            .new_wallet(name, &passphrase, WalletKind::Basic, None)
+            .new_wallet(
+                name,
+                &passphrase,
+                WalletKind::Basic,
+                HardwareKind::None,
+                None,
+            )
             .unwrap();
 
         let public_keys = vec![
@@ -421,7 +428,13 @@ mod default_wallet_transaction_builder_tests {
         let wallet_client = DefaultWalletClient::new_read_only(storage.clone());
 
         let (enckey, _) = wallet_client
-            .new_wallet(name, &passphrase, WalletKind::Basic, None)
+            .new_wallet(
+                name,
+                &passphrase,
+                WalletKind::Basic,
+                HardwareKind::None,
+                None,
+            )
             .unwrap();
 
         let public_keys = vec![

client-network/src/network_ops/default_network_ops_client.rs
@@ -557,6 +557,7 @@ mod tests {
     use client_common::tendermint::mock;
     use client_common::tendermint::types::*;
     use client_common::{seckey::derive_enckey, PrivateKey, PublicKey, Transaction};
+    use client_core::hd_wallet::HardwareKind;
     use client_core::service::HwKeyService;
     use client_core::signer::WalletSignerManager;
     use client_core::types::WalletKind;
@@ -787,7 +788,13 @@ mod tests {
         let transactions = vec![(input, output)];
 
         let (enckey, _) = wallet_client
-            .new_wallet(name, &passphrase, WalletKind::Basic, None)
+            .new_wallet(
+                name,
+                &passphrase,
+                WalletKind::Basic,
+                HardwareKind::None,
+                None,
+            )
             .unwrap();
 
         let tendermint_client = MockClient::default();
@@ -835,7 +842,13 @@ mod tests {
         let wallet_client = DefaultWalletClient::new_read_only(storage.clone());
 
         let (enckey, _) = wallet_client
-            .new_wallet(name, &passphrase, WalletKind::Basic, None)
+            .new_wallet(
+                name,
+                &passphrase,
+                WalletKind::Basic,
+                HardwareKind::None,
+                None,
+            )
             .unwrap();
 
         let tendermint_client = MockClient::default();
@@ -882,7 +895,13 @@ mod tests {
 
         let (enckey, _) = network_ops_client
             .get_wallet_client()
-            .new_wallet(name, &passphrase, WalletKind::Basic, None)
+            .new_wallet(
+                name,
+                &passphrase,
+                WalletKind::Basic,
+                HardwareKind::None,
+                None,
+            )
             .unwrap();
 
         let from_address = network_ops_client
@@ -941,7 +960,13 @@ mod tests {
 
         let (enckey, _) = network_ops_client
             .get_wallet_client()
-            .new_wallet(name, &passphrase, WalletKind::Basic, None)
+            .new_wallet(
+                name,
+                &passphrase,
+                WalletKind::Basic,
+                HardwareKind::None,
+                None,
+            )
             .unwrap();
 
         let from_address = network_ops_client
@@ -1010,7 +1035,13 @@ mod tests {
 
         let (enckey, _) = network_ops_client
             .get_wallet_client()
-            .new_wallet(name, &passphrase, WalletKind::Basic, None)
+            .new_wallet(
+                name,
+                &passphrase,
+                WalletKind::Basic,
+                HardwareKind::None,
+                None,
+            )
             .unwrap();
 
         assert_eq!(
@@ -1093,7 +1124,13 @@ mod tests {
 
         let (enckey, _) = network_ops_client
             .get_wallet_client()
-            .new_wallet(name, &passphrase, WalletKind::Basic, None)
+            .new_wallet(
+                name,
+                &passphrase,
+                WalletKind::Basic,
+                HardwareKind::None,
+                None,
+            )
             .unwrap();
 
         let from_address = network_ops_client
@@ -1144,7 +1181,13 @@ mod tests {
 
         let (enckey, _) = network_ops_client
             .get_wallet_client()
-            .new_wallet(name, &passphrase, WalletKind::Basic, None)
+            .new_wallet(
+                name,
+                &passphrase,
+                WalletKind::Basic,
+                HardwareKind::None,
+                None,
+            )
             .unwrap();
 
         let staking_account_address = network_ops_client

client-rpc/src/rpc/multisig_rpc.rs
@@ -346,6 +346,7 @@ mod test {
     use client_common::tendermint::Client;
     use client_common::TransactionObfuscation;
     use client_common::{PrivateKey, Result as CommonResult, SignedTransaction, Transaction};
+    use client_core::hd_wallet::HardwareKind;
     use client_core::service::HwKeyService;
     use client_core::signer::WalletSignerManager;
     use client_core::transaction_builder::DefaultWalletTransactionBuilder;
@@ -361,7 +362,13 @@ mod test {
 
         let (enckey, _) = multisig_rpc
             .client
-            .new_wallet(name, &passphrase, WalletKind::Basic, None)
+            .new_wallet(
+                name,
+                &passphrase,
+                WalletKind::Basic,
+                HardwareKind::None,
+                None,
+            )
             .unwrap();
         let wallet_request = WalletRequest {
             name: "Default".to_owned(),

dev-utils/src/commands/init_command.rs
@@ -20,6 +20,7 @@ use client_core::wallet::{DefaultWalletClient, WalletClient};
 
 use super::genesis_command::generate_genesis;
 use super::genesis_dev_config::GenesisDevConfig;
+use client_core::hd_wallet::HardwareKind;
 
 #[derive(Debug)]
 pub struct InitCommand {
@@ -349,14 +350,19 @@ impl InitCommand {
         let name = self.ask_string("please enter wallet name=", "my");
 
         let passphrase = InitCommand::ask_passphrase()?;
-        let enckey =
-            match wallet_client.new_wallet(&name.as_str(), &passphrase, WalletKind::Basic, None) {
-                Ok((enckey, _)) => enckey,
-                Err(reason) => {
-                    println!("new wallet fail={}", reason.to_string());
-                    return Ok(());
-                }
-            };
+        let enckey = match wallet_client.new_wallet(
+            &name.as_str(),
+            &passphrase,
+            WalletKind::Basic,
+            HardwareKind::None,
+            None,
+        ) {
+            Ok((enckey, _)) => enckey,
+            Err(reason) => {
+                println!("new wallet fail={}", reason.to_string());
+                return Ok(());
+            }
+        };
         success(&format!("Wallet created with name: {}", name));
 
         // main validator staking

crypto-com/thaler

client-cli/src/command.rs
@@ -45,7 +45,7 @@ use chain_core::tx::fee::LinearFee;
 use client_core::hd_wallet::HardwareKind;
 #[cfg(feature = "mock-hardware-wallet")]
 use client_core::service::MockHardwareService;
-use client_core::service::{HwKeyService, LedgerService};
+use client_core::service::{HwKeyService, LedgerService, WalletService};
 use once_cell::sync::Lazy;
 use std::env;
 
@@ -150,14 +150,6 @@ pub enum Command {
     Transaction {
         #[structopt(subcommand)]
         transaction_command: TransactionCommand,
-        #[structopt(
-            name = "hardware wallet type",
-            long = "hardware",
-            help = "Hardware wallet type",
-            possible_values = &HARDWARE_WALLET_KIND,
-            case_insensitive = false
-        )]
-        hardware: Option<HardwareKind>,
     },
     #[structopt(name = "state", about = "Get staked state of an address")]
     StakedState {
@@ -336,19 +328,22 @@ impl Command {
             }
             Command::Transaction {
                 transaction_command,
-                hardware,
             } => {
                 let storage = SledStorage::new(storage_path())?;
                 let tendermint_client = WebsocketRpcClient::new(&tendermint_url())?;
-                let hw_key_service = match hardware {
-                    None => HwKeyService::default(),
+                let wallet_name = transaction_command.wallet_name();
+                let wallet_service = WalletService::new(storage.clone());
+                let enckey = ask_seckey(None)?;
+                let wallet = wallet_service.get_wallet(&wallet_name, &enckey)?;
+                let hw_key_service = match wallet.hardware_kind {
                     #[cfg(feature = "mock-hardware-wallet")]
-                    Some(HardwareKind::Mock) => HwKeyService::Mock(MockHardwareService::new()),
-                    Some(HardwareKind::Trezor) => HwKeyService::default(),
-                    Some(HardwareKind::Ledger) => {
+                    HardwareKind::Mock => HwKeyService::Mock(MockHardwareService::new()),
+                    HardwareKind::Trezor => HwKeyService::default(),
+                    HardwareKind::Ledger => {
                         let ledger_service = LedgerService::new(true)?;
                         HwKeyService::Ledger(ledger_service)
                     }
+                    HardwareKind::LocalOnly => HwKeyService::default(),
                 };
                 let signer_manager =
                     WalletSignerManager::new(storage.clone(), hw_key_service.clone());
@@ -374,8 +369,11 @@ impl Command {
                     fee_algorithm,
                     transaction_obfuscation,
                 );
-                transaction_command
-                    .execute(network_ops_client.get_wallet_client(), &network_ops_client)
+                transaction_command.execute(
+                    network_ops_client.get_wallet_client(),
+                    &network_ops_client,
+                    enckey,
+                )
             }
             Command::StakedState {
                 name,
@@ -391,6 +389,7 @@ impl Command {
                         let ledger_service = LedgerService::new(true)?;
                         HwKeyService::Ledger(ledger_service)
                     }
+                    Some(HardwareKind::LocalOnly) => HwKeyService::default(),
                 };
                 let storage = SledStorage::new(storage_path())?;
                 let tendermint_client = WebsocketRpcClient::new(&tendermint_url())?;

client-cli/src/command/wallet_command.rs
@@ -6,7 +6,8 @@ use client_common::{Error, ErrorKind, PrivateKey, Result, ResultExt};
 use client_core::types::WalletKind;
 use client_core::{Mnemonic, WalletClient};
 
-use crate::{ask_passphrase, ask_seckey};
+use crate::{ask_hardware_kind, ask_passphrase, ask_seckey};
+use client_core::hd_wallet::HardwareKind;
 use client_core::service::WalletInfo;
 use client_core::wallet::WalletRequest;
 use std::fs::File;
@@ -162,8 +163,20 @@ impl WalletCommand {
                 "Passphrases do not match",
             ));
         }
-        let (enckey, mnemonic) =
-            wallet_client.new_wallet(name, &passphrase, wallet_kind, Some(mnemonics_word_count))?;
+
+        let hardware_kind = if wallet_kind == WalletKind::HW {
+            ask_hardware_kind(None)?
+        } else {
+            HardwareKind::LocalOnly
+        };
+
+        let (enckey, mnemonic) = wallet_client.new_wallet(
+            name,
+            &passphrase,
+            wallet_kind,
+            hardware_kind,
+            Some(mnemonics_word_count),
+        )?;
 
         if let WalletKind::HD = wallet_kind {
             ask("Please store following mnemonic safely to restore your wallet later: ");

client-core/src/signer/wallet_signer.rs
@@ -204,6 +204,7 @@ mod wallet_signer_tests {
     use chain_tx_validation::witness::verify_tx_address;
     use client_common::storage::MemoryStorage;
 
+    use crate::hd_wallet::HardwareKind;
     use crate::types::WalletKind;
     use crate::wallet::{DefaultWalletClient, WalletClient};
 
@@ -218,7 +219,13 @@ mod wallet_signer_tests {
         let wallet_client = DefaultWalletClient::new_read_only(storage.clone());
 
         let (enckey, _) = wallet_client
-            .new_wallet(name, &passphrase, WalletKind::Basic, None)
+            .new_wallet(
+                name,
+                &passphrase,
+                WalletKind::Basic,
+                HardwareKind::LocalOnly,
+                None,
+            )
             .unwrap();
 
         let public_keys = vec![
@@ -259,7 +266,13 @@ mod wallet_signer_tests {
         let wallet_client = DefaultWalletClient::new_read_only(storage.clone());
 
         let (enckey, _) = wallet_client
-            .new_wallet(name, &passphrase, WalletKind::Basic, None)
+            .new_wallet(
+                name,
+                &passphrase,
+                WalletKind::Basic,
+                HardwareKind::LocalOnly,
+                None,
+            )
             .unwrap();
 
         let public_keys = vec![

client-core/src/transaction_builder/default_wallet_transaction_builder.rs
@@ -239,6 +239,7 @@ mod default_wallet_transaction_builder_tests {
     use client_common::storage::MemoryStorage;
     use client_common::Transaction;
 
+    use crate::hd_wallet::HardwareKind;
     use crate::service::HwKeyService;
     use crate::signer::WalletSignerManager;
     use crate::types::WalletKind;
@@ -287,7 +288,13 @@ mod default_wallet_transaction_builder_tests {
         let wallet_client = DefaultWalletClient::new_read_only(storage.clone());
 
         let (enckey, _) = wallet_client
-            .new_wallet(name, &passphrase, WalletKind::Basic, None)
+            .new_wallet(
+                name,
+                &passphrase,
+                WalletKind::Basic,
+                HardwareKind::LocalOnly,
+                None,
+            )
             .unwrap();
 
         let public_keys = vec![
@@ -421,7 +428,13 @@ mod default_wallet_transaction_builder_tests {
         let wallet_client = DefaultWalletClient::new_read_only(storage.clone());
 
         let (enckey, _) = wallet_client
-            .new_wallet(name, &passphrase, WalletKind::Basic, None)
+            .new_wallet(
+                name,
+                &passphrase,
+                WalletKind::Basic,
+                HardwareKind::LocalOnly,
+                None,
+            )
             .unwrap();
 
         let public_keys = vec![

client-network/src/network_ops/default_network_ops_client.rs
@@ -557,6 +557,7 @@ mod tests {
     use client_common::tendermint::mock;
     use client_common::tendermint::types::*;
     use client_common::{seckey::derive_enckey, PrivateKey, PublicKey, Transaction};
+    use client_core::hd_wallet::HardwareKind;
     use client_core::service::HwKeyService;
     use client_core::signer::WalletSignerManager;
     use client_core::types::WalletKind;
@@ -787,7 +788,13 @@ mod tests {
         let transactions = vec![(input, output)];
 
         let (enckey, _) = wallet_client
-            .new_wallet(name, &passphrase, WalletKind::Basic, None)
+            .new_wallet(
+                name,
+                &passphrase,
+                WalletKind::Basic,
+                HardwareKind::LocalOnly,
+                None,
+            )
             .unwrap();
 
         let tendermint_client = MockClient::default();
@@ -835,7 +842,13 @@ mod tests {
         let wallet_client = DefaultWalletClient::new_read_only(storage.clone());
 
         let (enckey, _) = wallet_client
-            .new_wallet(name, &passphrase, WalletKind::Basic, None)
+            .new_wallet(
+                name,
+                &passphrase,
+                WalletKind::Basic,
+                HardwareKind::LocalOnly,
+                None,
+            )
             .unwrap();
 
         let tendermint_client = MockClient::default();
@@ -882,7 +895,13 @@ mod tests {
 
         let (enckey, _) = network_ops_client
             .get_wallet_client()
-            .new_wallet(name, &passphrase, WalletKind::Basic, None)
+            .new_wallet(
+                name,
+                &passphrase,
+                WalletKind::Basic,
+                HardwareKind::LocalOnly,
+                None,
+            )
             .unwrap();
 
         let from_address = network_ops_client
@@ -941,7 +960,13 @@ mod tests {
 
         let (enckey, _) = network_ops_client
             .get_wallet_client()
-            .new_wallet(name, &passphrase, WalletKind::Basic, None)
+            .new_wallet(
+                name,
+                &passphrase,
+                WalletKind::Basic,
+                HardwareKind::LocalOnly,
+                None,
+            )
             .unwrap();
 
         let from_address = network_ops_client
@@ -1010,7 +1035,13 @@ mod tests {
 
         let (enckey, _) = network_ops_client
             .get_wallet_client()
-            .new_wallet(name, &passphrase, WalletKind::Basic, None)
+            .new_wallet(
+                name,
+                &passphrase,
+                WalletKind::Basic,
+                HardwareKind::LocalOnly,
+                None,
+            )
             .unwrap();
 
         assert_eq!(
@@ -1093,7 +1124,13 @@ mod tests {
 
         let (enckey, _) = network_ops_client
             .get_wallet_client()
-            .new_wallet(name, &passphrase, WalletKind::Basic, None)
+            .new_wallet(
+                name,
+                &passphrase,
+                WalletKind::Basic,
+                HardwareKind::LocalOnly,
+                None,
+            )
             .unwrap();
 
         let from_address = network_ops_client
@@ -1144,7 +1181,13 @@ mod tests {
 
         let (enckey, _) = network_ops_client
             .get_wallet_client()
-            .new_wallet(name, &passphrase, WalletKind::Basic, None)
+            .new_wallet(
+                name,
+                &passphrase,
+                WalletKind::Basic,
+                HardwareKind::LocalOnly,
+                None,
+            )
             .unwrap();
 
         let staking_account_address = network_ops_client

client-rpc/src/rpc/multisig_rpc.rs
@@ -346,6 +346,7 @@ mod test {
     use client_common::tendermint::Client;
     use client_common::TransactionObfuscation;
     use client_common::{PrivateKey, Result as CommonResult, SignedTransaction, Transaction};
+    use client_core::hd_wallet::HardwareKind;
     use client_core::service::HwKeyService;
     use client_core::signer::WalletSignerManager;
     use client_core::transaction_builder::DefaultWalletTransactionBuilder;
@@ -361,7 +362,13 @@ mod test {
 
         let (enckey, _) = multisig_rpc
             .client
-            .new_wallet(name, &passphrase, WalletKind::Basic, None)
+            .new_wallet(
+                name,
+                &passphrase,
+                WalletKind::Basic,
+                HardwareKind::LocalOnly,
+                None,
+            )
             .unwrap();
         let wallet_request = WalletRequest {
             name: "Default".to_owned(),

dev-utils/src/commands/init_command.rs
@@ -20,6 +20,7 @@ use client_core::wallet::{DefaultWalletClient, WalletClient};
 
 use super::genesis_command::generate_genesis;
 use super::genesis_dev_config::GenesisDevConfig;
+use client_core::hd_wallet::HardwareKind;
 
 #[derive(Debug)]
 pub struct InitCommand {
@@ -349,14 +350,19 @@ impl InitCommand {
         let name = self.ask_string("please enter wallet name=", "my");
 
         let passphrase = InitCommand::ask_passphrase()?;
-        let enckey =
-            match wallet_client.new_wallet(&name.as_str(), &passphrase, WalletKind::Basic, None) {
-                Ok((enckey, _)) => enckey,
-                Err(reason) => {
-                    println!("new wallet fail={}", reason.to_string());
-                    return Ok(());
-                }
-            };
+        let enckey = match wallet_client.new_wallet(
+            &name.as_str(),
+            &passphrase,
+            WalletKind::Basic,
+            HardwareKind::LocalOnly,
+            None,
+        ) {
+            Ok((enckey, _)) => enckey,
+            Err(reason) => {
+                println!("new wallet fail={}", reason.to_string());
+                return Ok(());
+            }
+        };
         success(&format!("Wallet created with name: {}", name));
 
         // main validator staking